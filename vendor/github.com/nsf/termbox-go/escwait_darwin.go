01e897e6739a5dcdf9f2c3d82637ea29ec83d8e3
Glenn Vriesman <g.vriesman@livetech-systems.com>
2018-08-17 16:09:43 +0200

    Removed vendor packages

--- vendor/github.com/nsf/termbox-go/escwait_darwin.go
+++
@@ -1,9 +0,0 @@
-package termbox
-
-// On macOS, enable behavior which will wait before deciding that the escape
-// key was pressed, to account for partially send escape sequences, especially
-// with regard to lengthy mouse sequences.
-// See https://github.com/nsf/termbox-go/issues/132
-func enable_wait_for_escape_sequence() bool {
-	return true
-}
