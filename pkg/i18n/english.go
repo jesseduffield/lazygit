b42e917676a8c9dea311512052f0c39acd9a78c0
Glenn Vriesman <glenn.vriesman@gmail.com>
2018-09-04 15:11:52 +0200

    TAGS: Added default translation

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -31,6 +31,9 @@ func addEnglish(i18nObject *i18n.Bundle) error {
 			ID:    "BranchesTitle",
 			Other: "Branches",
 		}, &i18n.Message{
+			ID:    "TagsTitle",
+			Other: "Tags",
+		}, &i18n.Message{
 			ID:    "CommitsTitle",
 			Other: "Commits",
 		}, &i18n.Message{
