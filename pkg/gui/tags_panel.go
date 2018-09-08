9c5694f8c8fa64a91700516e1f5d30f03665fecd
Glenn Vriesman <glenn.vriesman@gmail.com>
2018-09-08 13:44:13 +0200

    TAGS: Started refreshTags

--- pkg/gui/tags_panel.go
+++ pkg/gui/tags_panel.go
@@ -1,1 +1,15 @@
 package gui
+
+import (
+	"github.com/jesseduffield/gocui"
+)
+
+// refreshTags will refresh the tags
+func (gui *Gui) refreshTags(g *gocui.Gui) error {
+
+	g.Update(func(g *gocui.Gui) error {
+		return nil
+	})
+
+	return nil
+}
