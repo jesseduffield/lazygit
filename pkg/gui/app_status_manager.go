d544632175b53de8cc0cdb14e561ad9914da96ee
Glenn Vriesman <glenn.vriesman@gmail.com>
2018-09-10 10:51:08 +0200

    STATUS: Cleaned, formatted, commented and changed performance

--- pkg/gui/app_status_manager.go
+++ pkg/gui/app_status_manager.go
@@ -2,43 +2,59 @@ package gui
 
 import "github.com/jesseduffield/lazygit/pkg/utils"
 
+// appStatus is used to store information about the status
+// of the current session
 type appStatus struct {
-	name       string
-	statusType string
-	duration   int
+	name       string `json:"name"`
+	statusType string `json:"status_type"`
+	duration   int    `json:"duration"`
 }
 
+// statusManager
 type statusManager struct {
 	statuses []appStatus
 }
 
+// removeStatus removes the status from the application.
+// name: the name of the status
 func (m *statusManager) removeStatus(name string) {
-	newStatuses := []appStatus{}
-	for _, status := range m.statuses {
+
+	for i, status := range m.statuses {
 		if status.name != name {
-			newStatuses = append(newStatuses, status)
+			m.statuses = append(m.statuses[:i], m.statuses[i+1:]...)
+			return
 		}
 	}
-	m.statuses = newStatuses
+
 }
 
+// addWaitingStatus creates a new status and adds it to the status
+// managers internal array
 func (m *statusManager) addWaitingStatus(name string) {
+
 	m.removeStatus(name)
+
 	newStatus := appStatus{
 		name:       name,
 		statusType: "waiting",
 		duration:   0,
 	}
+
 	m.statuses = append([]appStatus{newStatus}, m.statuses...)
 }
 
+// getStatus returns the string representing the status
 func (m *statusManager) getStatusString() string {
+
 	if len(m.statuses) == 0 {
 		return ""
 	}
+
 	topStatus := m.statuses[0]
+
 	if topStatus.statusType == "waiting" {
 		return topStatus.name + " " + utils.Loader()
 	}
+
 	return topStatus.name
 }
