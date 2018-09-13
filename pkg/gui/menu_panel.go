70c3dc6ca2ce8fa5b6276af11874728bd8f9c849
Glenn Vriesman <glenn.vriesman@gmail.com>
2018-09-13 15:22:20 +0200

    MENU: Comments, logging, checking, g passing

--- pkg/gui/menu_panel.go
+++ pkg/gui/menu_panel.go
@@ -8,47 +8,90 @@ import (
 	"github.com/jesseduffield/lazygit/pkg/utils"
 )
 
+// handleMenuPress is called when a user presses the menu key.
+// g and v are passed by the gocui library.
+// returns an error if something goes wrong.
 func (gui *Gui) handleMenuPress(g *gocui.Gui, v *gocui.View) error {
+
 	lineNumber := gui.getItemPosition(v)
 	if gui.State.Keys[lineNumber].Key == nil {
 		return nil
 	}
+
 	if len(gui.State.Keys) > lineNumber {
-		err := gui.handleMenuClose(g, v)
+
+		err := gui.handleMenuClose(gui.g, v)
 		if err != nil {
+			gui.Log.Errorf("Failed to handleMenuClose at handleMenuPress: %s\n", err)
 			return err
 		}
-		return gui.State.Keys[lineNumber].Handler(g, v)
+
+		vv, err := gui.g.View(gui.State.Keys[lineNumber].ViewName)
+		if err != nil {
+			gui.Log.Errorf("Failed to get view at handleMenuPress: %s\n", err)
+			return err
+		}
+
+		err = gui.State.Keys[lineNumber].Handler(gui.g, vv)
+		if err != nil {
+			gui.Log.Errorf("Failed to call handler for %s\n at handleMenuPress: %s\n", gui.State.Keys[lineNumber].Description, err)
+			return err
+		}
+
+		return nil
 	}
+
 	return nil
 }
 
-func (gui *Gui) handleMenuSelect(g *gocui.Gui, v *gocui.View) error {
-	// doing nothing for now
-	// but it is needed for switch in newLineFocused
+// handleMenuSelect doesn't do anything at the moment
+// but it is needed for switch in newLineFocused
+// TODO find use
+func (gui *Gui) handleMenuSelect(v *gocui.View) error {
 	return nil
 }
 
-func (gui *Gui) renderMenuOptions(g *gocui.Gui) error {
+// renderMenuOptions renders the menu options.
+// returns an error if something goes wrong.
+func (gui *Gui) renderMenuOptions() error {
+
 	optionsMap := map[string]string{
 		"esc/q": gui.Tr.SLocalize("close"),
 		"↑ ↓":   gui.Tr.SLocalize("navigate"),
 		"space": gui.Tr.SLocalize("execute"),
 	}
-	return gui.renderOptionsMap(g, optionsMap)
+
+	err := gui.renderOptionsMap(gui.g, optionsMap)
+	if err != nil {
+		gui.Log.Errorf("Failed to renderOptionsMap at renderMenuOptions: %s\n", err)
+		return err
+	}
+
+	return err
 }
 
+// handleMenuClose is called when the user closes the menu
+// g and v are passed by the gocui library.
+// returns an error if something goes wrong.
 func (gui *Gui) handleMenuClose(g *gocui.Gui, v *gocui.View) error {
-	// better to delete because for example after closing update confirmation panel,
-	// the focus isn't set back to any of panels and one is unable to even quit
-	//_, err := g.SetViewOnBottom(v.Name())
-	err := g.DeleteView("menu")
+
+	err := gui.g.DeleteView("menu")
 	if err != nil {
 		return err
 	}
-	return gui.returnFocus(g, v)
+
+	err = gui.returnFocus(gui.g, v)
+	if err != nil {
+		gui.Log.Errorf("Failed to return focus at handleMenuClose: %s\n", err)
+		return err
+	}
+
+	return nil
 }
 
+// GetKey returns the string representation of the key.
+// binding: what to get the key from.
+// returns the key
 func (gui *Gui) GetKey(binding Binding) string {
 	r, ok := binding.Key.(rune)
 	key := ""
@@ -62,22 +105,33 @@ func (gui *Gui) GetKey(binding Binding) string {
 	return key
 }
 
+// GetMaxKeyLength returns the size of a key from an array of bindings
+// bindings: what to get the keys from.
+// returns the size
 func (gui *Gui) GetMaxKeyLength(bindings []Binding) int {
+
 	max := 0
+
 	for _, binding := range bindings {
 		keyLength := len(gui.GetKey(binding))
 		if keyLength > max {
 			max = keyLength
 		}
 	}
+
 	return max
 }
 
+// handleMenu is called when a user opens the menu.
+// g and v are passed by the gocui library.
+// returns an error if something goes wrong.
 func (gui *Gui) handleMenu(g *gocui.Gui, v *gocui.View) error {
+
 	var (
 		contentGlobal, contentPanel   []string
 		bindingsGlobal, bindingsPanel []Binding
 	)
+
 	// clear keys slice, so we don't have ghost elements
 	gui.State.Keys = gui.State.Keys[:0]
 	bindings := gui.GetKeybindings()
@@ -110,22 +164,35 @@ func (gui *Gui) handleMenu(g *gocui.Gui, v *gocui.View) error {
 
 	// y1-1 so there will not be an extra space at the end of panel
 	x0, y0, x1, y1 := gui.getConfirmationPanelDimensions(contentJoined)
-	menuView, _ := g.SetView("menu", x0, y0, x1, y1-1, 0)
+	menuView, _ := gui.g.SetView("menu", x0, y0, x1, y1-1, 0)
 	menuView.Title = strings.Title(gui.Tr.SLocalize("menu"))
 	menuView.FgColor = gocui.ColorWhite
 
-	if err := gui.renderMenuOptions(g); err != nil {
+	err := gui.renderMenuOptions()
+	if err != nil {
+		gui.Log.Errorf("Failed to render menu options at handleMenu: %s\n", err)
 		return err
 	}
 
+	// TODO change this to renderString
 	fmt.Fprint(menuView, contentJoined)
 
-	g.Update(func(g *gocui.Gui) error {
-		_, err := g.SetViewOnTop("menu")
+	gui.g.Update(func(g *gocui.Gui) error {
+
+		_, err := gui.g.SetViewOnTop("menu")
 		if err != nil {
+			gui.Log.Errorf("Failed to setViewOnTop at handleMenu: %s\n", err)
 			return err
 		}
-		return gui.switchFocus(g, v, menuView)
+
+		err = gui.switchFocus(gui.g, v, menuView)
+		if err != nil {
+			gui.Log.Errorf("Failed to switchFocus at handleMenu")
+			return err
+		}
+
+		return nil
 	})
+
 	return nil
 }
