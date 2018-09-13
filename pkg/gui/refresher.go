8c38ff14473ce1acc278e39404c0958f0e5e3a26
Glenn Vriesman <glenn.vriesman@gmail.com>
2018-09-13 15:22:43 +0200

    REFRESH: Changed logging method

--- pkg/gui/refresher.go
+++ pkg/gui/refresher.go
@@ -10,21 +10,25 @@ var refreshables = make(map[string]func() error)
 // refresh refreshes the gui
 func (gui *Gui) refresh() {
 
-	for _, s := range refreshables {
+	for n, s := range refreshables {
 		err := s()
 		if err != nil {
-			gui.Log.Error(err.Error())
+			gui.Log.Errorf("Failed to refresh %s at refresh: %s\n", n, err)
 		}
 	}
+
+	return
 }
 
 // registerRefresher adds a new refresh job to be run when refresh is
-// called
+// called.
+// name: what to call the refresher.
+// f: what function to register
 func (gui *Gui) registerRefresher(name string, f func() error) error {
 
 	if refreshables[name] != nil {
 		err := errors.New(fmt.Sprintf("refresher %s already registered", name))
-		gui.Log.Error(err.Error())
+		gui.Log.Errorf("Failed to register refresher: %s\n", err)
 		return err
 	}
 
@@ -33,12 +37,14 @@ func (gui *Gui) registerRefresher(name string, f func() error) error {
 	return nil
 }
 
-// removeRefresher removes a refresh job
+// removeRefresher removes a refresh job.
+// name: what to remove.
+// returns an error if something goes wrong.
 func (gui *Gui) removeRefresher(name string) error {
 
 	if refreshables[name] == nil {
 		err := errors.New(fmt.Sprintf("refresher %s not registered", name))
-		gui.Log.Error(err.Error())
+		gui.Log.Errorf("Failed to remove refresher: %s\n", err)
 		return err
 	}
 
