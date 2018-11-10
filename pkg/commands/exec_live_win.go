18bcc0df4d28f84086e9f5ecb1771e6afc582fc9
mjarkk <mkopenga@gmail.com>
2018-11-10 08:43:02 +0100

    Fixed no error text on windows
    when executing a command live on windows it detects errors but did just show a empty string

--- pkg/commands/exec_live_win.go
+++ pkg/commands/exec_live_win.go
@@ -5,5 +5,6 @@ package commands
 // RunCommandWithOutputLiveWrapper runs a command live but because of windows compatibility this command can't be ran there
 // TODO: Remove this hack and replace it with a propper way to run commands live on windows
 func RunCommandWithOutputLiveWrapper(c *OSCommand, command string, output func(string) string) (errorMessage string, codeError error) {
-	return errorMessage, c.RunCommand(command)
+	cmdOputput := c.RunCommand(command)
+	return cmdOputput.Error(), cmdOputput
 }
