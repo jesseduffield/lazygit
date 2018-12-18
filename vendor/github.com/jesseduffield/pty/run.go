319064f040e028d75480ac0093225f23e81ec635
Jesse Duffield <jessedduffield@gmail.com>
2018-12-18 21:25:28 +1100

    switch to our own fork of pty which lets us set our own stdout and stderr

--- vendor/github.com/jesseduffield/pty/run.go
+++ vendor/github.com/jesseduffield/pty/run.go
@@ -12,11 +12,27 @@ import (
 // and c.Stderr, calls c.Start, and returns the File of the tty's
 // corresponding pty.
 func Start(c *exec.Cmd) (pty *os.File, err error) {
+	return StartWithSize(c, nil)
+}
+
+// StartWithSize assigns a pseudo-terminal tty os.File to c.Stdin, c.Stdout,
+// and c.Stderr, calls c.Start, and returns the File of the tty's
+// corresponding pty.
+//
+// This will resize the pty to the specified size before starting the command
+func StartWithSize(c *exec.Cmd, sz *Winsize) (pty *os.File, err error) {
 	pty, tty, err := Open()
 	if err != nil {
 		return nil, err
 	}
 	defer tty.Close()
+	if sz != nil {
+		err = Setsize(pty, sz)
+		if err != nil {
+			pty.Close()
+			return nil, err
+		}
+	}
 	c.Stdout = tty
 	c.Stdin = tty
 	c.Stderr = tty
