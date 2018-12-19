0afffd03cab29d133ab1458964732e29a66ae757
Jesse Duffield <jessedduffield@gmail.com>
2018-12-19 21:46:48 +1100

    remove comment

--- pkg/commands/exec_live_default.go
+++ pkg/commands/exec_live_default.go
@@ -19,7 +19,6 @@ import (
 // Output is a function that executes by every word that gets read by bufio
 // As return of output you need to give a string that will be written to stdin
 // NOTE: If the return data is empty it won't written anything to stdin
-// NOTE: You don't have to include a enter in the return data this function will do that for you
 func RunCommandWithOutputLiveWrapper(c *OSCommand, command string, output func(string) string) error {
 	splitCmd := str.ToArgv(command)
 	cmd := exec.Command(splitCmd[0], splitCmd[1:]...)
