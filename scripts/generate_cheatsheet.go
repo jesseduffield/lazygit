0dcfa09ff25384be1b926af85ba9a9a956681db4
Dawid Dziurla <dawidd0811@gmail.com>
2019-01-16 18:54:54 +0100

    run go fmt against generator

--- scripts/generate_cheatsheet.go
+++ scripts/generate_cheatsheet.go
@@ -9,55 +9,55 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jesseduffield/lazygit/pkg/app"
-    "github.com/jesseduffield/lazygit/pkg/config"
-    "log"
-    "os"
-    "strings"
+	"fmt"
+	"github.com/jesseduffield/lazygit/pkg/app"
+	"github.com/jesseduffield/lazygit/pkg/config"
+	"log"
+	"os"
+	"strings"
 )
 
 func writeString(file *os.File, str string) {
-    _, err := file.WriteString(str)
-    if err != nil {
-        log.Fatal(err)
-    }
+	_, err := file.WriteString(str)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func getTitle(mApp *app.App ,viewName string) string {
-    viewTitle := strings.Title(viewName) + "Title"
-    translatedTitle := mApp.Tr.SLocalize(viewTitle)
-    formattedTitle := fmt.Sprintf("\n## %s\n\n", translatedTitle)
-    return formattedTitle
+func getTitle(mApp *app.App, viewName string) string {
+	viewTitle := strings.Title(viewName) + "Title"
+	translatedTitle := mApp.Tr.SLocalize(viewTitle)
+	formattedTitle := fmt.Sprintf("\n## %s\n\n", translatedTitle)
+	return formattedTitle
 }
 
 func main() {
-    mConfig, _ := config.NewAppConfig("", "", "", "", "", new(bool))
-    mApp, _ := app.Setup(mConfig)
-    lang := mApp.Tr.GetLanguage()
-    file, _ := os.Create("Keybindings_" + lang + ".md")
-    current := ""
+	mConfig, _ := config.NewAppConfig("", "", "", "", "", new(bool))
+	mApp, _ := app.Setup(mConfig)
+	lang := mApp.Tr.GetLanguage()
+	file, _ := os.Create("Keybindings_" + lang + ".md")
+	current := ""
 
-    writeString(file, fmt.Sprintf("# Lazygit %s\n", mApp.Tr.SLocalize("menu")))
-    writeString(file, getTitle(mApp, "global"))
+	writeString(file, fmt.Sprintf("# Lazygit %s\n", mApp.Tr.SLocalize("menu")))
+	writeString(file, getTitle(mApp, "global"))
 
-    writeString(file, "<pre>\n")
+	writeString(file, "<pre>\n")
 
-    for _, binding := range mApp.Gui.GetKeybindings() {
-        if binding.Description == "" {
-            continue
-        }
+	for _, binding := range mApp.Gui.GetKeybindings() {
+		if binding.Description == "" {
+			continue
+		}
 
-        if binding.ViewName != current {
-            current = binding.ViewName
-            writeString(file, "</pre>\n")
-            writeString(file, getTitle(mApp, current))
-            writeString(file, "<pre>\n")
-        }
+		if binding.ViewName != current {
+			current = binding.ViewName
+			writeString(file, "</pre>\n")
+			writeString(file, getTitle(mApp, current))
+			writeString(file, "<pre>\n")
+		}
 
-        info := fmt.Sprintf("  <kbd>%s</kbd>: %s\n", binding.GetKey(), binding.Description)
-        writeString(file, info)
-    }
+		info := fmt.Sprintf("  <kbd>%s</kbd>: %s\n", binding.GetKey(), binding.Description)
+		writeString(file, info)
+	}
 
-    writeString(file, "</pre>\n")
+	writeString(file, "</pre>\n")
 }
