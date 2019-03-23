bd46e9c5b0d44796a6cb81b2426d28c845429abc
Jesse Duffield <jessedduffield@gmail.com>
2019-03-23 13:15:29 +1100

    delete menu keybinding before setting new one

--- pkg/gui/menu_panel.go
+++ pkg/gui/menu_panel.go
@@ -82,6 +82,8 @@ func (gui *Gui) createMenu(title string, items interface{}, itemCount int, handl
 	}
 
 	for _, key := range []gocui.Key{gocui.KeySpace, gocui.KeyEnter} {
+		_ = gui.g.DeleteKeybinding("menu", key, gocui.ModNone)
+
 		if err := gui.g.SetKeybinding("menu", key, gocui.ModNone, wrappedHandlePress); err != nil {
 			return err
 		}
