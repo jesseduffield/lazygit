3054afdc5274aa54cf6ae5b1d2a14afd3de374ab
Giorgio Previtera <gprevitera@equalexperts.com>
2019-07-07 19:15:11 +0100

    472 - Don't panic if not in a repository
    Display a friendly message and exit with an error if not
    in a Git repository. Using the same approach used in this PR:
    https://github.com/jesseduffield/lazydocker/pull/14/files

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -33,6 +33,11 @@ type App struct {
 	ClientContext string
 }
 
+type errorMapping struct {
+	originalError string
+	newError      string
+}
+
 func newProductionLogger(config config.AppConfigurer) *logrus.Logger {
 	log := logrus.New()
 	log.Out = ioutil.Discard
@@ -158,7 +163,8 @@ func (app *App) Run() error {
 		os.Exit(0)
 	}
 
-	return app.Gui.RunWithSubprocesses()
+	err := app.Gui.RunWithSubprocesses()
+	return err
 }
 
 // Rebase contains logic for when we've been run in demon mode, meaning we've
@@ -191,4 +197,23 @@ func (app *App) Close() error {
 		}
 	}
 	return nil
+}
+
+// KnownError takes an error and tells us whether it's an error that we know about where we can print a nicely formatted version of it rather than panicking with a stack trace
+func (app *App) KnownError(err error) (string, bool) {
+	errorMessage := err.Error()
+
+	mappings := []errorMapping{
+		{
+			originalError: "fatal: not a git repository (or any of the parent directories): .git",
+			newError:      app.Tr.SLocalize("notARepository"),
+		},
+	}
+
+	for _, mapping := range mappings {
+		if strings.Contains(errorMessage, mapping.originalError) {
+			return mapping.newError, true
+		}
+	}
+	return "", false
 }
