3a4d6608ebc32c8a10654f724ba4604277cec488
Jesse Duffield <jessedduffield@gmail.com>
2020-01-31 08:49:58 +1100

    fix keybinding issues with freebsd/openbsd

--- vendor/github.com/jesseduffield/termbox-go/api.go
+++ vendor/github.com/jesseduffield/termbox-go/api.go
@@ -2,13 +2,16 @@
 
 package termbox
 
-import "github.com/mattn/go-runewidth"
-import "fmt"
-import "os"
-import "os/signal"
-import "syscall"
-import "runtime"
-import "time"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
+
+	"github.com/mattn/go-runewidth"
+)
 
 // public API
 
@@ -26,13 +29,21 @@ func Init() error {
 
 	var err error
 
-	out, err = os.OpenFile("/dev/tty", syscall.O_WRONLY, 0)
-	if err != nil {
-		return err
-	}
-	in, err = syscall.Open("/dev/tty", syscall.O_RDONLY, 0)
-	if err != nil {
-		return err
+	if runtime.GOOS == "openbsd" || runtime.GOOS == "freebsd" {
+		out, err = os.OpenFile("/dev/tty", os.O_RDWR, 0)
+		if err != nil {
+			return err
+		}
+		in = int(out.Fd())
+	} else {
+		out, err = os.OpenFile("/dev/tty", os.O_WRONLY, 0)
+		if err != nil {
+			return err
+		}
+		in, err = syscall.Open("/dev/tty", syscall.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = setup_term()
