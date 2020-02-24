ce14193b3f4befe53378e44225fe79742c469442
Jesse Duffield <jessedduffield@gmail.com>
2020-02-25 09:09:17 +1100

    refresh commits when adding a tag

--- pkg/gui/tags_panel.go
+++ pkg/gui/tags_panel.go
@@ -106,7 +106,13 @@ func (gui *Gui) handleDeleteTag(g *gocui.Gui, v *gocui.View) error {
 		if err := gui.GitCommand.DeleteTag(tag.Name); err != nil {
 			return gui.createErrorPanel(gui.g, err.Error())
 		}
-		return gui.refreshTags()
+		if err := gui.refreshCommits(g); err != nil {
+			return gui.createErrorPanel(g, err.Error())
+		}
+		if err := gui.refreshTags(); err != nil {
+			return gui.createErrorPanel(g, err.Error())
+		}
+		return nil
 	}, nil)
 }
 
@@ -137,7 +143,13 @@ func (gui *Gui) handleCreateTag(g *gocui.Gui, v *gocui.View) error {
 		if err := gui.GitCommand.CreateLightweightTag(v.Buffer(), ""); err != nil {
 			return gui.createErrorPanel(gui.g, err.Error())
 		}
-		return gui.refreshTags()
+		if err := gui.refreshCommits(g); err != nil {
+			return gui.createErrorPanel(g, err.Error())
+		}
+		if err := gui.refreshTags(); err != nil {
+			return gui.createErrorPanel(g, err.Error())
+		}
+		return nil
 	})
 }
 
