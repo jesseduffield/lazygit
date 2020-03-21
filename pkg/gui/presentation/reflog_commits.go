98fbc6122150986ebd72bbf18a5a9b262ad932ce
Jesse Duffield <jessedduffield@gmail.com>
2020-03-21 12:18:26 +1100

    better formatted reflog list

---
+++ pkg/gui/presentation/reflog_commits.go
@@ -0,0 +1,37 @@
+package presentation
+
+import (
+	"github.com/fatih/color"
+	"github.com/jesseduffield/lazygit/pkg/commands"
+	"github.com/jesseduffield/lazygit/pkg/theme"
+	"github.com/jesseduffield/lazygit/pkg/utils"
+)
+
+func GetReflogCommitListDisplayStrings(commits []*commands.Commit, fullDescription bool) [][]string {
+	lines := make([][]string, len(commits))
+
+	var displayFunc func(*commands.Commit) []string
+	if fullDescription {
+		displayFunc = getFullDescriptionDisplayStringsForReflogCommit
+	} else {
+		displayFunc = getDisplayStringsForReflogCommit
+	}
+
+	for i := range commits {
+		lines[i] = displayFunc(commits[i])
+	}
+
+	return lines
+}
+
+func getFullDescriptionDisplayStringsForReflogCommit(c *commands.Commit) []string {
+	defaultColor := color.New(theme.DefaultTextColor)
+
+	return []string{utils.ColoredString(c.Sha[:8], color.FgBlue), utils.ColoredString(c.Date, color.FgMagenta), defaultColor.Sprint(c.Name)}
+}
+
+func getDisplayStringsForReflogCommit(c *commands.Commit) []string {
+	defaultColor := color.New(theme.DefaultTextColor)
+
+	return []string{utils.ColoredString(c.Sha[:8], color.FgBlue), defaultColor.Sprint(c.Name)}
+}
