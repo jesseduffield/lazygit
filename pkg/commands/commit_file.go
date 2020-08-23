942c8a3e0b9ed1450bd66eb050563fcd0a7a775f
Jesse Duffield <jessedduffield@gmail.com>
2020-08-23 14:20:28 +1000

    show file statuses in commit files view

--- pkg/commands/commit_file.go
+++ pkg/commands/commit_file.go
@@ -3,10 +3,13 @@ package commands
 // CommitFile : A git commit file
 type CommitFile struct {
 	// Parent is the identifier of the parent object e.g. a commit SHA if this commit file is for a commit, or a stash entry ref like 'stash@{1}'
-	Parent        string
-	Name          string
-	DisplayString string
-	Status        int // one of 'WHOLE' 'PART' 'NONE'
+	Parent string
+	Name   string
+
+	// Status tells us whether the file has been wholly or partially added to a patch. We might want to pull this logic up into the gui package and make it a map like we do with cherry picked commits
+	Status int // one of 'WHOLE' 'PART' 'NONE'
+
+	ChangeStatus string // e.g. 'A' for added or 'M' for modified. This is based on the result from git diff --name-status
 }
 
 func (f *CommitFile) ID() string {
