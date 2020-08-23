bd9579983e8d6276d39e10dcc9045fbe65ce45f8
Jesse Duffield <jessedduffield@gmail.com>
2020-08-23 17:40:42 +1000

    bump gocui to ensure no crash on startup

---
+++ vendor/github.com/go-errors/errors/error_backward.go
@@ -0,0 +1,22 @@
+// +build !go1.13
+
+package errors
+
+// Is detects whether the error is equal to a given error. Errors
+// are considered equal by this function if they are the same object,
+// or if they both contain the same error inside an errors.Error.
+func Is(e error, original error) bool {
+	if e == original {
+		return true
+	}
+
+	if e, ok := e.(*Error); ok {
+		return Is(e.Err, original)
+	}
+
+	if original, ok := original.(*Error); ok {
+		return Is(e, original.Err)
+	}
+
+	return false
+}
