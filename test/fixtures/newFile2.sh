40c5cd4b4b3436da2616e848d40e7760ec99167f
Jesse Duffield <jessedduffield@gmail.com>
2020-10-05 20:18:53 +1100

    another integration test

---
+++ test/fixtures/newFile2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git init
+
+git config user.email "CI@example.com"
+git config user.name "CI"
+
+echo "line 1" > myfile1
+git add .
+git commit -am "myfile1"
+
+echo "line 2" >> myfile1
+echo "line 3" >> myfile1
+echo "line 4" >> myfile1
