50dd7b00c35b2c1e049b329c3a3baca5bc3e6efa
1jz <philipgolovin@gmail.com>
2021-02-11 22:51:57 -0500

    add colors to differentiate action and menu commands

--- pkg/gui/options_menu_panel.go
+++ pkg/gui/options_menu_panel.go
@@ -3,6 +3,7 @@ package gui
 import (
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/jesseduffield/gocui"
 	"github.com/jesseduffield/lazygit/pkg/utils"
 )
@@ -33,6 +34,17 @@ func (gui *Gui) getBindings(v *gocui.View) []*Binding {
 	return append(bindingsPanel, bindingsGlobal...)
 }
 
+func (gui *Gui) displayDescription(binding *Binding) string {
+	commandColor := color.New(color.FgCyan)
+	menuColor := color.New(color.FgMagenta)
+
+	if binding.OpensMenu {
+		return menuColor.Sprintf("%s...", binding.Description)
+	}
+
+	return commandColor.Sprint(binding.Description)
+}
+
 func (gui *Gui) handleCreateOptionsMenu(g *gocui.Gui, v *gocui.View) error {
 	bindings := gui.getBindings(v)
 
@@ -41,7 +53,7 @@ func (gui *Gui) handleCreateOptionsMenu(g *gocui.Gui, v *gocui.View) error {
 	for i, binding := range bindings {
 		binding := binding // note to self, never close over loop variables
 		menuItems[i] = &menuItem{
-			displayStrings: []string{GetKeyDisplay(binding.Key), binding.Description},
+			displayStrings: []string{GetKeyDisplay(binding.Key), gui.displayDescription(binding)},
 			onPress: func() error {
 				if binding.Key == nil {
 					return nil
