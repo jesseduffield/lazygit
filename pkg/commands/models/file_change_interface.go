e01c9c85d63efef9e04e3bbdd95ff85c26625b1c
Jesse Duffield <jessedduffield@gmail.com>
2021-03-21 15:01:13 +1100

    remove collapsed field

--- pkg/commands/models/file_change_interface.go
+++ pkg/commands/models/file_change_interface.go
@@ -1,5 +1,6 @@
 package models
 
+// sometimes we need to deal with either a node (which contains a file) or an actual file
 type IFileChange interface {
 	GetHasUnstagedChanges() bool
 	GetHasStagedChanges() bool
