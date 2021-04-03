6f8eb91611962d48cd8a2d7d19c7b910e3f78fae
Jesse Duffield <jessedduffield@gmail.com>
2021-04-03 21:56:42 +1100

    WIP

--- pkg/gui/recording.go
+++ pkg/gui/recording.go
@@ -109,8 +109,11 @@ func (gui *Gui) loadRecordedEvents() ([]RecordedEvent, error) {
 
 	err = json.Unmarshal(data, &events)
 	if err != nil {
+		panic(err)
 		return nil, err
 	}
+
+	panic(events)
 
 	return events, nil
 }
@@ -120,8 +123,11 @@ func (gui *Gui) saveRecordedEvents() error {
 		return nil
 	}
 
+	gui.Log.Warn(gui.RecordedEvents)
+
 	jsonEvents, err := json.Marshal(gui.RecordedEvents)
 	if err != nil {
+		panic(err)
 		return err
 	}
 
