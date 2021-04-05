b0f3bfef27dbd9ec8aed72c6cb89040c0f59868d
Jesse Duffield <jessedduffield@gmail.com>
2021-04-05 13:13:20 +1000

    see how CI goes running these tests in parallel

--- pkg/gui/gui_test.go
+++ pkg/gui/gui_test.go
@@ -180,6 +180,8 @@ func Test(t *testing.T) {
 		test := test
 
 		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+
 			speeds := getTestSpeeds(test.Speed, updateSnapshots)
 
 			for i, speed := range speeds {
