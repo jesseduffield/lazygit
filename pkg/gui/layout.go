013964e5d9eae5c728f9dfb2126055ee973b296e
Jesse Duffield <jessedduffield@gmail.com>
2021-04-11 21:42:18 +1000

    print header for command log

--- pkg/gui/layout.go
+++ pkg/gui/layout.go
@@ -1,8 +1,12 @@
 package gui
 
 import (
+	"fmt"
+
+	"github.com/fatih/color"
 	"github.com/jesseduffield/gocui"
 	"github.com/jesseduffield/lazygit/pkg/theme"
+	"github.com/jesseduffield/lazygit/pkg/utils"
 )
 
 const SEARCH_PREFIX = "search: "
@@ -121,12 +125,21 @@ func (gui *Gui) createAllViews() error {
 	gui.Views.Extras.Title = gui.Tr.CommandLog
 	gui.Views.Extras.FgColor = theme.GocuiDefaultTextColor
 	gui.Views.Extras.Autoscroll = true
+	gui.printCommandLogHeader()
 
 	if _, err := gui.g.SetCurrentView(gui.defaultSideContext().GetViewName()); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (gui *Gui) printCommandLogHeader() {
+	introStr := fmt.Sprintf(
+		gui.Tr.CommandLogHeader,
+		gui.getKeyDisplay(gui.Config.GetUserConfig().Keybinding.Universal.ExtrasMenu),
+	)
+	fmt.Fprintln(gui.Views.Extras, utils.ColoredString(introStr, color.FgCyan))
 }
 
 // layout is called for every screen re-render e.g. when the screen is resized
