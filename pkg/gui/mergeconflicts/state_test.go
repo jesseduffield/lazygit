e478c254d43495ecfa2dad55df1ca2d2726ff974
Harrison Jones <harrison@hhj.me>
2021-05-20 12:51:26 -0400

    Handle alternate merge conflict format; add tests

---
+++ pkg/gui/mergeconflicts/state_test.go
@@ -0,0 +1,90 @@
+package mergeconflicts
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindConflicts(t *testing.T) {
+	type scenario struct {
+		name     string
+		content  string
+		expected []*mergeConflict
+	}
+
+	scenarios := []scenario{
+		{
+			name:     "empty",
+			content:  "",
+			expected: []*mergeConflict{},
+		},
+		{
+			name: "various conflicts",
+			content: `++<<<<<<< HEAD
+foo
+++=======
+bar
+++>>>>>>> branch
+
+<<<<<<< HEAD: foo/bar/baz.go
+foo
+bar
+=======
+baz
+>>>>>>> branch
+
+++<<<<<<< MERGE_HEAD
+foo
+++=======
+bar
+++>>>>>>> branch
+
+++<<<<<<< Updated upstream
+foo
+++=======
+bar
+++>>>>>>> branch
+
+++<<<<<<< ours
+foo
+++=======
+bar
+++>>>>>>> branch
+`,
+			expected: []*mergeConflict{
+				{
+					start:  0,
+					middle: 2,
+					end:    4,
+				},
+				{
+					start:  6,
+					middle: 9,
+					end:    11,
+				},
+				{
+					start:  13,
+					middle: 15,
+					end:    17,
+				},
+				{
+					start:  19,
+					middle: 21,
+					end:    23,
+				},
+				{
+					start:  25,
+					middle: 27,
+					end:    29,
+				},
+			},
+		},
+	}
+
+	for _, s := range scenarios {
+		t.Run(s.name, func(t *testing.T) {
+			assert.EqualValues(t, s.expected, findConflicts(s.content))
+		})
+	}
+}
