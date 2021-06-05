36e5fa2b1479888eb19c1dbb5e52c59cd8bc0da7
Jesse Duffield <jessedduffield@gmail.com>
2021-06-05 15:56:50 +1000

    more refactoring
    WIP
    
    WIP

--- pkg/commands/models/branch.go
+++ pkg/commands/models/branch.go
@@ -24,3 +24,26 @@ func (b *Branch) ID() string {
 func (b *Branch) Description() string {
 	return b.RefName()
 }
+
+// this method does not consider the case where the git config states that a branch is tracking the config.
+// The Pullables value here is based on whether or not we saw an upstream when doing `git branch`
+func (b *Branch) IsTrackingRemote() bool {
+	return b.IsRealBranch() && b.Pullables != "?"
+}
+
+func (b *Branch) MatchesUpstream() bool {
+	return b.IsRealBranch() && b.Pushables == "0" && b.Pullables == "0"
+}
+
+func (b *Branch) HasCommitsToPush() bool {
+	return b.IsRealBranch() && b.Pushables != "0"
+}
+
+func (b *Branch) HasCommitsToPull() bool {
+	return b.IsRealBranch() && b.Pullables != "0"
+}
+
+// for when we're in a detached head state
+func (b *Branch) IsRealBranch() bool {
+	return b.Pushables != "" && b.Pullables != ""
+}
