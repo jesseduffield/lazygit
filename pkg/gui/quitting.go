107503c90301a1bfd035f61a5155c572e800f5af
Davyd McColl <davydm@gmail.com>
2021-06-09 20:47:29 +0200

    :art: alternative syntax

--- pkg/gui/quitting.go
+++ pkg/gui/quitting.go
@@ -31,13 +31,13 @@ func (gui *Gui) handleQuitWithoutChangingDirectory() error {
 
 func (gui *Gui) toggleWhitespaceInDiffView() error {
 	gui.State.IgnoreWhitespaceInDiffView = !gui.State.IgnoreWhitespaceInDiffView
-	var toastMessage string
+
+	toastMessage := gui.Tr.ShowingWhitespaceInDiffView
 	if gui.State.IgnoreWhitespaceInDiffView {
 		toastMessage = gui.Tr.IgnoringWhitespaceInDiffView
-	} else {
-		toastMessage = gui.Tr.ShowingWhitespaceInDiffView
 	}
 	gui.raiseToast(toastMessage)
+
 	return gui.refreshFilesAndSubmodules()
 }
 
