a4c0bef2eedee737034c442a30f082d490383e05
Jesse Duffield <jessedduffield@gmail.com>
2021-06-15 14:14:18 +1000

    minor test
    second line

--- pkg/commands/stash_entries_test.go
+++ pkg/commands/stash_entries_test.go
@@ -1,35 +1,35 @@
 package commands
 
-import (
-	"os/exec"
-	"testing"
+// import (
+// 	"os/exec"
+// 	"testing"
 
-	"github.com/jesseduffield/lazygit/pkg/secureexec"
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/jesseduffield/lazygit/pkg/secureexec"
+// 	"github.com/stretchr/testify/assert"
+// )
 
-// TestGitCommandStashDo is a function.
-func TestGitCommandStashDo(t *testing.T) {
-	gitCmd := NewDummyGit()
-	gitCmd.GetOSCommand().Command = func(cmd string, args ...string) *exec.Cmd {
-		assert.EqualValues(t, "git", cmd)
-		assert.EqualValues(t, []string{"stash", "drop", "stash@{1}"}, args)
+// // TestGitCommandStashDo is a function.
+// func TestGitCommandStashDo(t *testing.T) {
+// 	gitCmd := NewDummyGit()
+// 	gitCmd.GetOSCommand().Command = func(cmd string, args ...string) *exec.Cmd {
+// 		assert.EqualValues(t, "git", cmd)
+// 		assert.EqualValues(t, []string{"stash", "drop", "stash@{1}"}, args)
 
-		return secureexec.Command("echo")
-	}
+// 		return secureexec.Command("echo")
+// 	}
 
-	assert.NoError(t, gitCmd.StashDo(1, "drop"))
-}
+// 	assert.NoError(t, gitCmd.StashDo(1, "drop"))
+// }
 
-// TestGitCommandStashSave is a function.
-func TestGitCommandStashSave(t *testing.T) {
-	gitCmd := NewDummyGit()
-	gitCmd.GetOSCommand().Command = func(cmd string, args ...string) *exec.Cmd {
-		assert.EqualValues(t, "git", cmd)
-		assert.EqualValues(t, []string{"stash", "save", "A stash message"}, args)
+// // TestGitCommandStashSave is a function.
+// func TestGitCommandStashSave(t *testing.T) {
+// 	gitCmd := NewDummyGit()
+// 	gitCmd.GetOSCommand().Command = func(cmd string, args ...string) *exec.Cmd {
+// 		assert.EqualValues(t, "git", cmd)
+// 		assert.EqualValues(t, []string{"stash", "save", "A stash message"}, args)
 
-		return secureexec.Command("echo")
-	}
+// 		return secureexec.Command("echo")
+// 	}
 
-	assert.NoError(t, gitCmd.StashSave("A stash message"))
-}
+// 	assert.NoError(t, gitCmd.StashSave("A stash message"))
+// }
