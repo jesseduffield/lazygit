c21493b0e91ee03b755f6caa04cb93d5d45675e1
Jesse Duffield <jessedduffield@gmail.com>
2022-01-18 19:44:41 +1100

    add integration test for deleting a range of lines in the staging panel

---
+++ test/integration/stagingTwo/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd $1
+
+git init
+
+git config user.email "CI@example.com"
+git config user.name "CI"
+
+cp ../files/one.txt one.txt
+git add .
+git commit -am file1
+
+cp ../files/one_new.txt one.txt
