9538d26219dbcd16a08a1e6074e4081958a240c8
Benjamin DIDIER <contact@aziks.aleeas.com>
2022-05-13 00:12:51 +0200

    wip: add french intro popup message translation

---
+++ pkg/i18n/french.go
@@ -0,0 +1,22 @@
+package i18n
+
+const frenchIntroPopupMessage = `
+Merci d'utiliser lazygit! 3 choses que je souhaite te partager:
+
+ 1) Pour en apprendre plus sur les fonctionnalitées de lazygit, regarde
+			cette vidéo (en anglais):
+      https://youtu.be/CPLdltN7wgE
+
+ 2) Les notes de mise à jour sont disponible en suivant ce lien:
+      https://github.com/jesseduffield/lazygit/releases
+
+ 3) Si tu utilises git, alors tu es un développeur! Avec ton aide, nous pouvons
+		améliorer laygit, donc n'hésite pas à nous en rejoindre en devenant un
+		contributeur:
+      https://github.com/jesseduffield/lazygit
+		Tu peux aussi me sponsoriser et me dire ce sur quoi tu souhaiterais que je
+		travaille en suivant ce lien:
+			https://github.com/sponsors/jesseduffield
+		Ou même donner une étoile au dépôt git pour montrer ton soutiens!
+`
+
