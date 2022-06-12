3067c2c321c46260bcfb571ff82a9fcecb0c0a7b
John Shin <shinhs0506@gmail.com>
2022-06-11 23:18:29 -0700

    support open file and link on WSL

--- pkg/config/config_linux.go
+++ pkg/config/config_linux.go
@@ -1,7 +1,27 @@
 package config
 
+import (
+    "io/ioutil"
+    "strings"
+)
+
+func isWSL() bool {
+	data, err := ioutil.ReadFile("/proc/sys/kernel/osrelease");
+    return err == nil && strings.Contains(string(data), "microsoft")
+}
+
 // GetPlatformDefaultConfig gets the defaults for the platform
 func GetPlatformDefaultConfig() OSConfig {
+
+    if isWSL() {
+        return OSConfig{
+            EditCommand:         ``,
+            EditCommandTemplate: "",
+            OpenCommand:         `powershell.exe start explorer.exe {{filename}} >/dev/null`,
+            OpenLinkCommand:     `powershell.exe start  {{link}} >/dev/null`,
+        }
+    }
+
 	return OSConfig{
 		EditCommand:         ``,
 		EditCommandTemplate: "",
