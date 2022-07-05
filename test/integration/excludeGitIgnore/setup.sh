e1047037b9d6d00a81843c8203ba0be14e85ac29
Juan Sanchez Montalvo <jsmontalvo@osx-l-07779.youview.local>
2022-07-05 09:02:40 +0100

    Add git user and email to the integration test

--- test/integration/excludeGitIgnore/setup.sh
+++ test/integration/excludeGitIgnore/setup.sh
@@ -4,6 +4,9 @@ set -e
 
 cd $1
 
+git config user.email "CI@example.com"
+git config user.name "CI"
+
 git init
 
 
--- test/integration/gitignoreMenu/setup.sh
+++ test/integration/gitignoreMenu/setup.sh
@@ -3,6 +3,8 @@
 set -e
 
 cd $1
+git config user.email "CI@example.com"
+git config user.name "CI"
 
 git init
 
