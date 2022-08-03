64224e7caaad745f988c7e153f74a5b5bf2e5077
Luka Markušić <markusicluka@gmail.com>
2022-08-03 13:57:26 +0200

    Update pkg/commands/oscommands/os.go
    Co-authored-by: Ryoga <eial5q265e5+github@gmail.com>

--- pkg/commands/oscommands/os.go
+++ pkg/commands/oscommands/os.go
@@ -120,17 +120,17 @@ func (c *OSCommand) AppendLineToFile(filename, line string) error {
 		return utils.WrapError(err)
 	}
 
-	buf := make([]byte, 1)
 	if info.Size() > 0 {
 		// read last char
+		buf := make([]byte, 1)
 		if _, err := f.ReadAt(buf, info.Size()-1); err != nil {
 			return utils.WrapError(err)
 		}
-	}
 
-	// if the last byte of the file is not a newline, add it
-	if []byte("\n")[0] != buf[0] {
-		_, err = f.WriteString("\n")
+		// if the last byte of the file is not a newline, add it
+		if []byte("\n")[0] != buf[0] {
+			_, err = f.WriteString("\n")
+		}
 	}
 
 	if err == nil {
