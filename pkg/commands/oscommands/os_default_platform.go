83b3e5f4184dabb902e5691373f37c85a3b9f189
Luka Markušić <luka.markusic@microblink.com>
2022-08-07 18:37:48 +0200

    Detect the user's shell for running custom commands
    This makes it possible to use any shell, and by adding the `-i` flag it
    makes it interactive (running aliases and functions is possible now)

--- pkg/commands/oscommands/os_default_platform.go
+++ pkg/commands/oscommands/os_default_platform.go
@@ -4,14 +4,23 @@
 package oscommands
 
 import (
+	"os"
 	"runtime"
 )
+
+func getShell() string {
+	defaultShell := "bash"
+	if shell := os.Getenv("SHELL"); shell != defaultShell {
+		return shell
+	}
+	return defaultShell
+}
 
 func GetPlatform() *Platform {
 	return &Platform{
 		OS:              runtime.GOOS,
-		Shell:           "bash",
-		ShellArg:        "-c",
+		Shell:           getShell(),
+		ShellArg:        "-ic",
 		OpenCommand:     "open {{filename}}",
 		OpenLinkCommand: "open {{link}}",
 	}
