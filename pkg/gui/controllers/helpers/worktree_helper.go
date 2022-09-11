db02c13bf65a251cab3a92409024b05813cd1ec6
Joel Baranick <joel.baranick@ensighten.com>
2022-09-10 22:36:47 -0700

    Address PR comments

--- pkg/gui/controllers/helpers/worktree_helper.go
+++ pkg/gui/controllers/helpers/worktree_helper.go
@@ -1,6 +1,15 @@
 package helpers
 
-import "github.com/jesseduffield/lazygit/pkg/gui/types"
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+
+	"github.com/jesseduffield/lazygit/pkg/commands/models"
+	"github.com/jesseduffield/lazygit/pkg/gui/types"
+)
 
 type IWorktreeHelper interface {
 	GetMainWorktreeName() string
@@ -27,6 +36,25 @@ func (self *WorktreeHelper) GetMainWorktreeName() string {
 	return ""
 }
 
+func (self *WorktreeHelper) IsCurrentWorktree(w *models.Worktree) bool {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	return pwd == w.Path
+}
+
+func (self *WorktreeHelper) IsWorktreePathMissing(w *models.Worktree) bool {
+	if _, err := os.Stat(w.Path); err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return true
+		}
+		log.Fatalln(fmt.Errorf("failed to check if worktree path `%s` exists\n%w", w.Path, err).Error())
+	}
+	return false
+}
+
 func (self *WorktreeHelper) NewWorktree() error {
 	return self.c.Prompt(types.PromptOpts{
 		Title: self.c.Tr.NewWorktreePath,
@@ -35,30 +63,7 @@ func (self *WorktreeHelper) NewWorktree() error {
 			if err := self.c.Git().Worktree.New(sanitizedBranchName(response)); err != nil {
 				return err
 			}
-
-			//if self.c.CurrentContext() != self.contexts.Worktrees {
-			//	if err := self.c.PushContext(self.contexts.Worktrees); err != nil {
-			//		return err
-			//	}
-			//}
-
-			// self.contexts.LocalCommits.SetSelectedLineIdx(0)
-			// self.contexts.Branches.SetSelectedLineIdx(0)
-
 			return self.c.Refresh(types.RefreshOptions{Mode: types.ASYNC})
 		},
 	})
 }
-
-//func (self *WorktreeHelper) GetCurrentWorktreeName() string {
-//	for _, worktree := range self.c.Model().Worktrees {
-//		if worktree.Current() {
-//			if worktree.Main() {
-//				return ""
-//			}
-//			return worktree.Name()
-//		}
-//	}
-//
-//	return ""
-//}
