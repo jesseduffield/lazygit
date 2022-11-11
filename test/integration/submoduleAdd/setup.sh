52316e628e651dc038a9c04ab6fd262aabcf60cb
Jesse Duffield <jessedduffield@gmail.com>
2022-11-12 10:18:02 +1100

    fix broken CI (see https://vielmetti.typepad.com/logbook/2022/10/git-security-fixes-lead-to-fatal-transport-file-not-allowed-error-in-ci-systems-cve-2022-39253.html)
    try this
    
    WIP

--- test/integration/submoduleAdd/setup.sh
+++ test/integration/submoduleAdd/setup.sh
@@ -11,6 +11,11 @@ git init
 
 git config user.email "CI@example.com"
 git config user.name "CI"
+# see https://vielmetti.typepad.com/logbook/2022/10/git-security-fixes-lead-to-fatal-transport-file-not-allowed-error-in-ci-systems-cve-2022-39253.html
+# NOTE: I don't think this actually works if it's only applied to the repo.
+# On CI we set the global setting, but given it's a security concern I don't want
+# people to do that for their locals.
+git config protocol.file.allow always
 
 echo test1 > myfile1
 git add .
