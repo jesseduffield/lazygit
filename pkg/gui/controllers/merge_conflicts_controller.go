39c20bc63454390d9c00bfb8536e44e04afd55ca
Ryooooooga <eial5q265e5@gmail.com>
2023-02-11 21:05:55 +0900

    chore: change to work `return-alt1` on all views

--- pkg/gui/controllers/merge_conflicts_controller.go
+++ pkg/gui/controllers/merge_conflicts_controller.go
@@ -110,6 +110,11 @@ func (self *MergeConflictsController) GetKeybindings(opts types.KeybindingsOpts)
 			Handler:     self.Escape,
 			Description: self.c.Tr.ReturnToFilesPanel,
 		},
+		{
+			Key:         opts.GetKey(opts.Config.Universal.ReturnAlt1),
+			Handler:     self.Escape,
+			Description: self.c.Tr.ReturnToFilesPanel,
+		},
 	}
 
 	return bindings
