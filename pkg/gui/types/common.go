d675117289cc195b7cae105a808a29383f48d9d4
Stefan Haller <stefan@haller-berlin.de>
2023-04-13 11:13:57 +0200

    Fix activation of initial context
    This broke with 40f6767cfc77; the symptom is that starting lazygit with a git
    arg (e.g. "lazygit log") wouldn't activate the requested panel correctly. While
    it would show in the expanded view, it didn't have a green frame, and keyboard
    events would go to the files panel.

--- pkg/gui/types/common.go
+++ pkg/gui/types/common.go
@@ -45,6 +45,7 @@ type IGuiCommon interface {
 	CurrentContext() Context
 	CurrentStaticContext() Context
 	IsCurrentContext(Context) bool
+	ActivateContext(context Context) error
 	// enters search mode for the current view
 	OpenSearch()
 
