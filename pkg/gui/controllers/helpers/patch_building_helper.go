jesseduffield/lazygit

commit 8edad826caf2fa48bfad33f9f8c4f3ba49a052da

Begin refactoring gui
This begins a big refactor of moving more code out of the Gui struct into contexts, controllers, and helpers. We also move some code into structs in the
gui package purely for the sake of better encapsulation

pkg/gui/controllers/helpers/patch_building_helper.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jesseduffield/lazygit/pkg/commands"
 	"github.com/jesseduffield/lazygit/pkg/commands/types/enums"
 	"github.com/jesseduffield/lazygit/pkg/gui/context"
+	"github.com/jesseduffield/lazygit/pkg/gui/patch_exploring"
 	"github.com/jesseduffield/lazygit/pkg/gui/types"
 )
 
@@ -59,4 +60,60 @@ func (self *PatchBuildingHelper) Reset() error {
 
 	// refreshing the current context so that the secondary panel is hidden if necessary.
 	return self.c.PostRefreshUpdate(self.c.CurrentContext())
+}
+
+func (self *PatchBuildingHelper) RefreshPatchBuildingPanel(opts types.OnFocusOpts) error {
+	selectedLineIdx := -1
+	if opts.ClickedWindowName == "main" {
+		selectedLineIdx = opts.ClickedViewLineIdx
+	}
+
+	if !self.git.Patch.PatchBuilder.Active() {
+		return self.Escape()
+	}
+
+	// get diff from commit file that's currently selected
+	path := self.contexts.CommitFiles.GetSelectedPath()
+	if path == "" {
+		return nil
+	}
+
+	ref := self.contexts.CommitFiles.CommitFileTreeViewModel.GetRef()
+	to := ref.RefName()
+	from, reverse := self.c.Modes().Diffing.GetFromAndReverseArgsForDiff(ref.ParentRefName())
+	diff, err := self.git.WorkingTree.ShowFileDiff(from, to, reverse, path, true, self.c.State().GetIgnoreWhitespaceInDiffView())
+	if err != nil {
+		return err
+	}
+
+	secondaryDiff := self.git.Patch.PatchBuilder.RenderPatchForFile(path, false, false)
+	if err != nil {
+		return err
+	}
+
+	context := self.contexts.CustomPatchBuilder
+
+	oldState := context.GetState()
+
+	state := patch_exploring.NewState(diff, selectedLineIdx, oldState, self.c.Log)
+	context.SetState(state)
+	if state == nil {
+		return self.Escape()
+	}
+
+	mainContent := context.GetContentToRender(true)
+
+	self.contexts.CustomPatchBuilder.FocusSelection()
+
+	return self.c.RenderToMainViews(types.RefreshMainOpts{
+		Pair: self.c.MainViewPairs().PatchBuilding,
+		Main: &types.ViewUpdateOpts{
+			Task:  types.NewRenderStringWithoutScrollTask(mainContent),
+			Title: self.c.Tr.Patch,
+		},
+		Secondary: &types.ViewUpdateOpts{
+			Task:  types.NewRenderStringWithoutScrollTask(secondaryDiff),
+			Title: self.c.Tr.CustomPatch,
+		},
+	})
 }

commit a2778f01c661468caaf2aa2c3476f58e9925dbc4

Disregard the "ignore whitespace" option in the patch building panel
It's not possible to reliably stage things into a custom patch when "ignore
whitespace" is on, so always treat it as off here (like we do in the staging
panel).

It looks like this is a regression that was introduced in 8edad826ca.

pkg/gui/controllers/helpers/patch_building_helper.go
@@ -73,7 +73,9 @@ func (self *PatchBuildingHelper) RefreshPatchBuildingPanel(opts types.OnFocusOpt
 	ref := self.c.Contexts().CommitFiles.CommitFileTreeViewModel.GetRef()
 	to := ref.RefName()
 	from, reverse := self.c.Modes().Diffing.GetFromAndReverseArgsForDiff(ref.ParentRefName())
-	diff, err := self.c.Git().WorkingTree.ShowFileDiff(from, to, reverse, path, true, self.c.State().GetIgnoreWhitespaceInDiffView())
+	// Passing false for ignoreWhitespace because the patch building panel
+	// doesn't work when whitespace is ignored
+	diff, err := self.c.Git().WorkingTree.ShowFileDiff(from, to, reverse, path, true, false)
 	if err != nil {
 		return err
 	}