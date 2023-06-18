a50e313d1ebc8d1a008bc7e0d460867c5dd2ff05
AzraelSec <federicogerardi94@gmail.com>
2023-06-18 16:30:18 +0200

    test: integrate remote tag deletion in integration tag-related tests

--- pkg/integration/tests/tag/crud_annotated.go
+++ pkg/integration/tests/tag/crud_annotated.go
@@ -12,6 +12,7 @@ var CrudAnnotated = NewIntegrationTest(NewIntegrationTestArgs{
 	SetupConfig:  func(config *config.AppConfig) {},
 	SetupRepo: func(shell *Shell) {
 		shell.EmptyCommit("initial commit")
+		shell.CloneIntoRemote("origin")
 	},
 	Run: func(t *TestDriver, keys config.KeybindingConfig) {
 		t.Views().Tags().
@@ -31,11 +32,53 @@ var CrudAnnotated = NewIntegrationTest(NewIntegrationTestArgs{
 			Lines(
 				MatchesRegexp(`new-tag.*message`).IsSelected(),
 			).
+			Press(keys.Universal.Push).
+			Tap(func() {
+				t.ExpectPopup().Prompt().
+					Title(Equals("Remote to push tag 'new-tag' to:")).
+					InitialText(Equals("origin")).
+					SuggestionLines(
+						Contains("origin"),
+					).
+					Confirm()
+			}).
 			Press(keys.Universal.Remove).
 			Tap(func() {
-				t.ExpectPopup().Confirmation().
-					Title(Equals("Delete tag")).
-					Content(Equals("Are you sure you want to delete tag 'new-tag'?")).
+				t.ExpectPopup().
+					Menu().
+					Title(Equals("Delete tag 'new-tag'?")).
+					Select(Contains("Delete remote tag")).
+					Confirm()
+			}).
+			Tap(func() {
+				t.ExpectPopup().Prompt().
+					Title(Equals("Remote from which to remove tag 'new-tag':")).
+					InitialText(Equals("origin")).
+					SuggestionLines(
+						Contains("origin"),
+					).
+					Confirm()
+			}).
+			Tap(func() {
+				t.ExpectPopup().
+					Confirmation().
+					Title(Equals("Delete tag 'new-tag'?")).
+					Content(Equals("Are you sure you want to delete the remote tag 'new-tag' from 'origin'?")).
+					Confirm()
+			}).
+			Lines(
+				MatchesRegexp(`new-tag.*message`).IsSelected(),
+			).
+			Tap(func() {
+				t.Git().
+					RemoteTagDeleted("origin", "new-tag")
+			}).
+			Press(keys.Universal.Remove).
+			Tap(func() {
+				t.ExpectPopup().
+					Menu().
+					Title(Equals("Delete tag 'new-tag'?")).
+					Select(Contains("Delete local tag")).
 					Confirm()
 			}).
 			IsEmpty().
--- pkg/integration/tests/tag/crud_lightweight.go
+++ pkg/integration/tests/tag/crud_lightweight.go
@@ -12,6 +12,7 @@ var CrudLightweight = NewIntegrationTest(NewIntegrationTestArgs{
 	SetupConfig:  func(config *config.AppConfig) {},
 	SetupRepo: func(shell *Shell) {
 		shell.EmptyCommit("initial commit")
+		shell.CloneIntoRemote("origin")
 	},
 	Run: func(t *TestDriver, keys config.KeybindingConfig) {
 		t.Views().Tags().
@@ -36,11 +37,53 @@ var CrudLightweight = NewIntegrationTest(NewIntegrationTestArgs{
 					).
 					PressEscape()
 			}).
+			Press(keys.Universal.Push).
+			Tap(func() {
+				t.ExpectPopup().Prompt().
+					Title(Equals("Remote to push tag 'new-tag' to:")).
+					InitialText(Equals("origin")).
+					SuggestionLines(
+						Contains("origin"),
+					).
+					Confirm()
+			}).
 			Press(keys.Universal.Remove).
 			Tap(func() {
-				t.ExpectPopup().Confirmation().
-					Title(Equals("Delete tag")).
-					Content(Equals("Are you sure you want to delete tag 'new-tag'?")).
+				t.ExpectPopup().
+					Menu().
+					Title(Equals("Delete tag 'new-tag'?")).
+					Select(Contains("Delete remote tag")).
+					Confirm()
+			}).
+			Tap(func() {
+				t.ExpectPopup().Prompt().
+					Title(Equals("Remote from which to remove tag 'new-tag':")).
+					InitialText(Equals("origin")).
+					SuggestionLines(
+						Contains("origin"),
+					).
+					Confirm()
+			}).
+			Tap(func() {
+				t.ExpectPopup().
+					Confirmation().
+					Title(Equals("Delete tag 'new-tag'?")).
+					Content(Equals("Are you sure you want to delete the remote tag 'new-tag' from 'origin'?")).
+					Confirm()
+			}).
+			Lines(
+				MatchesRegexp(`new-tag.*initial commit`).IsSelected(),
+			).
+			Tap(func() {
+				t.Git().
+					RemoteTagDeleted("origin", "new-tag")
+			}).
+			Press(keys.Universal.Remove).
+			Tap(func() {
+				t.ExpectPopup().
+					Menu().
+					Title(Equals("Delete tag 'new-tag'?")).
+					Select(Contains("Delete local tag")).
 					Confirm()
 			}).
 			IsEmpty()
