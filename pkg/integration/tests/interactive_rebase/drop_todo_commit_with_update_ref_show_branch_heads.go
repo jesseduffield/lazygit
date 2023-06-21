e2146a2932232e7a07ca1cfe6e93fd26811cc2e2
Stefan Haller <stefan@haller-berlin.de>
2023-06-21 21:45:03 +0200

    Improve updateRef tests
    These tests not only test the correct handling and display of the updateRef
    command, but also the visualization of branch heads in the commits panel. Since
    we are about to change the behavior here, extend the tests so that a master
    commit is added (we don't want this to be visualized as a branch head), and then
    a stack of two non-main branches. At the end of this branch we only want to
    visualize the head commit of the first.

--- pkg/integration/tests/interactive_rebase/drop_todo_commit_with_update_ref_show_branch_heads.go
+++ pkg/integration/tests/interactive_rebase/drop_todo_commit_with_update_ref_show_branch_heads.go
@@ -15,9 +15,11 @@ var DropTodoCommitWithUpdateRefShowBranchHeads = NewIntegrationTest(NewIntegrati
 	},
 	SetupRepo: func(shell *Shell) {
 		shell.
-			CreateNCommits(3).
-			NewBranch("mybranch").
-			CreateNCommitsStartingAt(3, 4)
+			CreateNCommits(1).
+			NewBranch("branch1").
+			CreateNCommitsStartingAt(3, 2).
+			NewBranch("branch2").
+			CreateNCommitsStartingAt(3, 5)
 
 		shell.SetConfig("rebase.updateRefs", "true")
 	},
@@ -25,26 +27,28 @@ var DropTodoCommitWithUpdateRefShowBranchHeads = NewIntegrationTest(NewIntegrati
 		t.Views().Commits().
 			Focus().
 			Lines(
-				Contains("(*) commit 06").IsSelected(),
+				Contains("(*) commit 07").IsSelected(),
+				Contains("commit 06"),
 				Contains("commit 05"),
-				Contains("commit 04"),
-				Contains("(*) commit 03"),
+				Contains("(*) commit 04"),
+				Contains("commit 03"),
 				Contains("commit 02"),
-				Contains("commit 01"),
+				Contains("(*) commit 01"),
 			).
-			NavigateToLine(Contains("commit 01")).
+			NavigateToLine(Contains("commit 02")).
 			Press(keys.Universal.Edit).
 			Focus().
 			Lines(
-				Contains("pick").Contains("(*) commit 06"),
+				Contains("pick").Contains("(*) commit 07"),
+				Contains("pick").Contains("commit 06"),
 				Contains("pick").Contains("commit 05"),
-				Contains("pick").Contains("commit 04"),
-				Contains("update-ref").Contains("master"),
-				Contains("pick").Contains("(*) commit 03"),
-				Contains("pick").Contains("commit 02"),
-				Contains("<-- YOU ARE HERE --- commit 01"),
+				Contains("update-ref").Contains("branch1"),
+				Contains("pick").Contains("(*) commit 04"),
+				Contains("pick").Contains("commit 03"),
+				Contains("<-- YOU ARE HERE --- commit 02"),
+				Contains("(*) commit 01"),
 			).
-			NavigateToLine(Contains("commit 05")).
+			NavigateToLine(Contains("commit 06")).
 			Press(keys.Universal.Remove)
 
 		t.Common().ContinueRebase()
@@ -52,11 +56,12 @@ var DropTodoCommitWithUpdateRefShowBranchHeads = NewIntegrationTest(NewIntegrati
 		t.Views().Commits().
 			IsFocused().
 			Lines(
-				Contains("(*) commit 06"),
-				Contains("commit 04"),
-				Contains("(*) commit 03"),
+				Contains("(*) commit 07"),
+				Contains("commit 05"),
+				Contains("(*) commit 04"),
+				Contains("commit 03"),
 				Contains("commit 02"),
-				Contains("commit 01"),
+				Contains("(*) commit 01"),
 			)
 	},
 })
