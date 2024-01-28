jesseduffield/lazygit

commit 240948b882dffffeb98d2ccecc4241faf258f6f5

Return only visible views from TopViewInWindow
Without this it's not reliably possible to ask whether a given view is visible
by asking

  windowHelper.TopViewInWindow(context.GetWindowName()) == context.GetView()

because there could be transient, invisible contexts after it in the Z order.

I guess it's a bit of a coincidence that this has never been a problem so far.

pkg/gui/controllers/helpers/window_helper.go
@@ -92,7 +92,7 @@ func (self *WindowHelper) MoveToTopOfWindow(context types.Context) {
 
 	topView := self.TopViewInWindow(window)
 
-	if view.Name() != topView.Name() {
+	if topView != nil && view.Name() != topView.Name() {
 		if err := self.c.GocuiGui().SetViewOnTopOf(view.Name(), topView.Name()); err != nil {
 			self.c.Log.Error(err)
 		}
@@ -106,7 +106,7 @@ func (self *WindowHelper) TopViewInWindow(windowName string) *gocui.View {
 	// The views list is ordered highest-last, so we're grabbing the last view of the window
 	var topView *gocui.View
 	for _, currentView := range self.c.GocuiGui().Views() {
-		if lo.Contains(viewNamesInWindow, currentView.Name()) {
+		if lo.Contains(viewNamesInWindow, currentView.Name()) && currentView.Visible {
 			topView = currentView
 		}
 	}

pkg/gui/main_panels.go
@@ -39,12 +39,8 @@ func (gui *Gui) moveMainContextToTop(context types.Context) {
 	view := context.GetView()
 
 	topView := gui.helpers.Window.TopViewInWindow(context.GetWindowName())
-	if topView == nil {
-		gui.Log.Error("unexpected: topView is nil")
-		return
-	}
 
-	if topView != view {
+	if topView != nil && topView != view {
 		// We need to copy the content to avoid a flicker effect: If we're flicking
 		// through files in the files panel, we use a different view to render the
 		// files vs the directories, and if you select dir A, then file B, then dir

commit c66667c8c1d68758d31e265acfc3fdf7aff4f8c0

Fix main view refresh after adding the first file to a custom patch
This broke with 240948b.

pkg/gui/controllers/helpers/inline_status_helper.go
@@ -66,7 +66,7 @@ func (self inlineStatusHelperTask) Continue() {
 func (self *InlineStatusHelper) WithInlineStatus(opts InlineStatusOpts, f func(gocui.Task) error) {
 	context := self.c.ContextForKey(opts.ContextKey).(types.IListContext)
 	view := context.GetView()
-	visible := view.Visible && self.windowHelper.TopViewInWindow(context.GetWindowName()) == view
+	visible := view.Visible && self.windowHelper.TopViewInWindow(context.GetWindowName(), false) == view
 	if visible && context.IsItemVisible(opts.Item) {
 		self.c.OnWorker(func(task gocui.Task) {
 			self.start(opts)

pkg/gui/controllers/helpers/window_helper.go
@@ -90,7 +90,7 @@ func (self *WindowHelper) MoveToTopOfWindow(context types.Context) {
 
 	window := context.GetWindowName()
 
-	topView := self.TopViewInWindow(window)
+	topView := self.TopViewInWindow(window, true)
 
 	if topView != nil && view.Name() != topView.Name() {
 		if err := self.c.GocuiGui().SetViewOnTopOf(view.Name(), topView.Name()); err != nil {
@@ -99,14 +99,14 @@ func (self *WindowHelper) MoveToTopOfWindow(context types.Context) {
 	}
 }
 
-func (self *WindowHelper) TopViewInWindow(windowName string) *gocui.View {
+func (self *WindowHelper) TopViewInWindow(windowName string, includeInvisibleViews bool) *gocui.View {
 	// now I need to find all views in that same window, via contexts. And I guess then I need to find the index of the highest view in that list.
 	viewNamesInWindow := self.viewNamesInWindow(windowName)
 
 	// The views list is ordered highest-last, so we're grabbing the last view of the window
 	var topView *gocui.View
 	for _, currentView := range self.c.GocuiGui().Views() {
-		if lo.Contains(viewNamesInWindow, currentView.Name()) && currentView.Visible {
+		if lo.Contains(viewNamesInWindow, currentView.Name()) && (currentView.Visible || includeInvisibleViews) {
 			topView = currentView
 		}
 	}

pkg/gui/main_panels.go
@@ -38,7 +38,7 @@ func (gui *Gui) moveMainContextToTop(context types.Context) {
 
 	view := context.GetView()
 
-	topView := gui.helpers.Window.TopViewInWindow(context.GetWindowName())
+	topView := gui.helpers.Window.TopViewInWindow(context.GetWindowName(), true)
 
 	if topView != nil && topView != view {
 		// We need to copy the content to avoid a flicker effect: If we're flicking