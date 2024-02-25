7d4e8debe9f2cd58d4975c21a4a369e7cd596040
Sai Chand <saichand.kolloju@gmail.com>
2024-02-25 00:19:22 -0500

    add localized strings

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -97,6 +97,8 @@ type TranslationSet struct {
 	SureForceCheckout                     string
 	ForceCheckoutBranch                   string
 	BranchName                            string
+	BranchURL                             string
+	PullRequestURL                        string
 	NewBranchNameBranchOff                string
 	CantDeleteCheckOutBranch              string
 	DeleteBranchTitle                     string
@@ -567,6 +569,7 @@ type TranslationSet struct {
 	CopyCommitAttributeToClipboard        string
 	CopyCommitAttributeToClipboardTooltip string
 	CopyBranchNameToClipboard             string
+	CopyBranchAttributeToClipboard        string
 	CopyPathToClipboard                   string
 	CommitPrefixPatternError              string
 	CopySelectedTextToClipboard           string
@@ -620,6 +623,8 @@ type TranslationSet struct {
 	ExtrasTitle                          string
 	PushingTagStatus                     string
 	PullRequestURLCopiedToClipboard      string
+	BranchNameCopiedToClipboard          string
+	BranchURLCopiedToClipboard           string
 	CommitDiffCopiedToClipboard          string
 	CommitSHACopiedToClipboard           string
 	CommitURLCopiedToClipboard           string
@@ -909,6 +914,9 @@ type Actions struct {
 	Undo                              string
 	Redo                              string
 	CopyPullRequestURL                string
+	CopyBranchName                    string
+	CopyBranchURL                     string
+	CopyBranchAttributeToClipboard    string
 	OpenDiffTool                      string
 	OpenMergeTool                     string
 	OpenCommitInBrowser               string
@@ -1041,6 +1049,8 @@ func EnglishTranslationSet() TranslationSet {
 		SureForceCheckout:                   "Are you sure you want force checkout? You will lose all local changes",
 		ForceCheckoutBranch:                 "Force checkout branch",
 		BranchName:                          "Branch name",
+		BranchURL:                           "Branch URL",
+		PullRequestURL:                      "Pull request URL",
 		NewBranchNameBranchOff:              "New branch name (branch is off of '{{.branchName}}')",
 		CantDeleteCheckOutBranch:            "You cannot delete the checked out branch!",
 		DeleteBranchTitle:                   "Delete branch '{{.selectedBranchName}}'?",
@@ -1514,6 +1524,7 @@ func EnglishTranslationSet() TranslationSet {
 		CopyCommitAttributeToClipboard:        "Copy commit attribute to clipboard",
 		CopyCommitAttributeToClipboardTooltip: "Copy commit attribute to clipboard (e.g. hash, URL, diff, message, author).",
 		CopyBranchNameToClipboard:             "Copy branch name to clipboard",
+		CopyBranchAttributeToClipboard:        "Copy branch attribute to clipboard",
 		CopyPathToClipboard:                   "Copy path to clipboard",
 		CopySelectedTextToClipboard:           "Copy selected text to clipboard",
 		CommitPrefixPatternError:              "Error in commitPrefix pattern",
@@ -1566,6 +1577,8 @@ func EnglishTranslationSet() TranslationSet {
 		ExtrasTitle:                           "Command log",
 		PushingTagStatus:                      "Pushing tag",
 		PullRequestURLCopiedToClipboard:       "Pull request URL copied to clipboard",
+		BranchNameCopiedToClipboard:           "Branch name copied to clipboard",
+		BranchURLCopiedToClipboard:            "Branch URL copied to clipboard",
 		CommitDiffCopiedToClipboard:           "Commit diff copied to clipboard",
 		CommitSHACopiedToClipboard:            "Commit SHA copied to clipboard",
 		CommitURLCopiedToClipboard:            "Commit URL copied to clipboard",
@@ -1746,6 +1759,9 @@ func EnglishTranslationSet() TranslationSet {
 			CopyCommitURLToClipboard:       "Copy commit URL to clipboard",
 			CopyCommitAuthorToClipboard:    "Copy commit author to clipboard",
 			CopyCommitAttributeToClipboard: "Copy to clipboard",
+			CopyBranchAttributeToClipboard: "Copy to clipboard",
+			CopyBranchName:                 "Copy branch name",
+			CopyBranchURL:                  "Copy branch URL",
 			CopyPatchToClipboard:           "Copy patch to clipboard",
 			MoveCommitUp:                   "Move commit up",
 			MoveCommitDown:                 "Move commit down",
