a0c9e455ae3c9312d29299a461abb49d69d60d44
Stefan Haller <stefan@haller-berlin.de>
2024-03-08 21:19:10 +0100

    Support setting a range of commits to "edit" outside of a rebase
    It starts a rebase on the bottom-most commit of the range, and sets all the
    selected commits to "edit" (skipping merge commits, because they can't be
    edited).

--- pkg/integration/tests/test_list.go
+++ pkg/integration/tests/test_list.go
@@ -167,6 +167,7 @@ var tests = []*components.IntegrationTest{
 	interactive_rebase.DropWithCustomCommentChar,
 	interactive_rebase.EditFirstCommit,
 	interactive_rebase.EditNonTodoCommitDuringRebase,
+	interactive_rebase.EditRangeSelectOutsideRebase,
 	interactive_rebase.EditTheConflCommit,
 	interactive_rebase.FixupFirstCommit,
 	interactive_rebase.FixupSecondCommit,
