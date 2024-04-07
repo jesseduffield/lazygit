d8c1eb879b5138627559cd62515bdaa657005ece
hongkuang <liurenhong@outlook.com>
2024-04-07 16:09:26 +0800

    pkg: fix some comment
    Signed-off-by: hongkuang <liurenhong@outlook.com>

--- pkg/gui/dummies.go
+++ pkg/gui/dummies.go
@@ -8,13 +8,13 @@ import (
 	"github.com/jesseduffield/lazygit/pkg/utils"
 )
 
-// NewDummyGui creates a new dummy GUI for testing
 func NewDummyUpdater() *updates.Updater {
 	newAppConfig := config.NewDummyAppConfig()
 	dummyUpdater, _ := updates.NewUpdater(utils.NewDummyCommon(), newAppConfig, oscommands.NewDummyOSCommand())
 	return dummyUpdater
 }
 
+// NewDummyGui creates a new dummy GUI for testing
 func NewDummyGui() *Gui {
 	newAppConfig := config.NewDummyAppConfig()
 	dummyGui, _ := NewGui(utils.NewDummyCommon(), newAppConfig, &git_commands.GitVersion{}, NewDummyUpdater(), false, "", nil)
