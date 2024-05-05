jesseduffield/lazygit

commit e1ceb6892a138b85b50a3270a5451f75f01f99ab

Disable deadlock reporting when debugging an integration test

pkg/gui/gui.go
@@ -623,7 +623,10 @@ func (gui *Gui) Run(startArgs appTypes.StartArgs) error {
 	deadlock.Opts.LogBuf = utils.NewOnceWriter(os.Stderr, func() {
 		gui.g.Close()
 	})
-	deadlock.Opts.Disable = !gui.Debug
+	// disable deadlock reporting if we're not running in debug mode, or if
+	// we're debugging an integration test. In this latter case, stopping at
+	// breakpoints and stepping through code can easily take more than 30s.
+	deadlock.Opts.Disable = !gui.Debug || os.Getenv(components.WAIT_FOR_DEBUGGER_ENV_VAR) == ""
 
 	if err := gui.Config.ReloadUserConfig(); err != nil {
 		return nil

commit 10e29ce7dd8ca0105684032953411051ed542273

Fix deadlock reporting
Deadlock reporting broke in e1ceb6892a; since then, it was *off* when running
debug builds normally, but *on* when debugging an integration test. Both of
which are exactly opposite of what we want.

pkg/gui/gui.go
@@ -662,7 +662,7 @@ func (gui *Gui) Run(startArgs appTypes.StartArgs) error {
 	// disable deadlock reporting if we're not running in debug mode, or if
 	// we're debugging an integration test. In this latter case, stopping at
 	// breakpoints and stepping through code can easily take more than 30s.
-	deadlock.Opts.Disable = !gui.Debug || os.Getenv(components.WAIT_FOR_DEBUGGER_ENV_VAR) == ""
+	deadlock.Opts.Disable = !gui.Debug || os.Getenv(components.WAIT_FOR_DEBUGGER_ENV_VAR) != ""
 
 	if err := gui.Config.ReloadUserConfig(); err != nil {
 		return nil