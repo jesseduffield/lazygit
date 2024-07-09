0c5f1f4f773962ab6a23e85d98fdf1e77c97ed99
Brandon <brandondong96@gmail.com>
2024-07-08 18:52:05 -0700

    Simplify test

--- pkg/integration/tests/file/stage_deleted_range_select.go
+++ pkg/integration/tests/file/stage_deleted_range_select.go
@@ -12,64 +12,39 @@ var StageDeletedRangeSelect = NewIntegrationTest(NewIntegrationTestArgs{
 	SetupConfig: func(config *config.AppConfig) {
 	},
 	SetupRepo: func(shell *Shell) {
-		shell.CreateFileAndAdd("dir1/file-a", "")
-		shell.CreateFileAndAdd("dir2/file-b", "")
-		shell.CreateFileAndAdd("file-c", "")
-		shell.CreateFileAndAdd("file-d", "")
+		shell.CreateFileAndAdd("file-a", "")
+		shell.CreateFileAndAdd("file-b", "")
 		shell.Commit("first commit")
 
-		shell.DeleteFile("dir1/file-a")
-		shell.DeleteFile("dir2/file-b")
-		shell.DeleteFile("file-c")
-		shell.DeleteFile("file-d")
+		shell.DeleteFile("file-a")
+		shell.DeleteFile("file-b")
 	},
 	Run: func(t *TestDriver, keys config.KeybindingConfig) {
 		t.Views().Files().
 			IsFocused().
 			Lines(
-				Contains("▼ dir1").IsSelected(),
-				Contains("   D").Contains("file-a"),
-				Contains("▼ dir2"),
-				Contains("   D").Contains("file-b"),
-				Contains(" D").Contains("file-c"),
-				Contains(" D").Contains("file-d"),
+				Contains(" D").Contains("file-a").IsSelected(),
+				Contains(" D").Contains("file-b"),
 			).
+			// Stage a single deleted file
+			PressPrimaryAction().
+			Lines(
+				Contains("D ").Contains("file-a").IsSelected(),
+				Contains(" D").Contains("file-b"),
+			).
+			Press(keys.Universal.ToggleRangeSelect).
 			NavigateToLine(Contains("file-b")).
-			Press(keys.Universal.ToggleRangeSelect).
-			NavigateToLine(Contains("file-c")).
-			// Stage a deleted file and nested file
+			// Stage both files while a deleted file is already staged
 			PressPrimaryAction().
 			Lines(
-				Contains("▼ dir1"),
-				Contains("   D").Contains("file-a"),
-				Contains("▼ dir2"),
-				Contains("  D ").Contains("file-b").IsSelected(),
-				Contains("D ").Contains("file-c").IsSelected(),
-				Contains(" D").Contains("file-d"),
-			).
-			Press(keys.Universal.ToggleRangeSelect).
-			NavigateToLine(Contains("file-a")).
-			Press(keys.Universal.ToggleRangeSelect).
-			NavigateToLine(Contains("file-d")).
-			// Stage the entire selection of files while some deleted files are already staged
-			PressPrimaryAction().
-			Lines(
-				Contains("▼ dir1"),
-				Contains("  D ").Contains("file-a").IsSelected(),
-				Contains("▼ dir2").IsSelected(),
-				Contains("  D ").Contains("file-b").IsSelected(),
-				Contains("D ").Contains("file-c").IsSelected(),
-				Contains("D ").Contains("file-d").IsSelected(),
+				Contains("D ").Contains("file-a").IsSelected(),
+				Contains("D ").Contains("file-b").IsSelected(),
 			).
 			// Unstage; back to everything being unstaged
 			PressPrimaryAction().
 			Lines(
-				Contains("▼ dir1"),
-				Contains("   D").Contains("file-a").IsSelected(),
-				Contains("▼ dir2").IsSelected(),
-				Contains("   D").Contains("file-b").IsSelected(),
-				Contains(" D").Contains("file-c").IsSelected(),
-				Contains(" D").Contains("file-d").IsSelected(),
+				Contains(" D").Contains("file-a").IsSelected(),
+				Contains(" D").Contains("file-b").IsSelected(),
 			)
 	},
 })
