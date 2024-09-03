da7a28c1175e74a5ee73fd272632da9c56f9448b
Dmytro Suvorov <992467+susl@users.noreply.github.com>
2024-09-03 17:48:37 +0300

    Add Zed to user_config.go and schema

--- pkg/config/user_config.go
+++ pkg/config/user_config.go
@@ -546,7 +546,7 @@ type OSConfig struct {
 
 	// A built-in preset that sets all of the above settings. Supported presets
 	// are defined in the getPreset function in editor_presets.go.
-	EditPreset string `yaml:"editPreset,omitempty" jsonschema:"example=vim,example=nvim,example=emacs,example=nano,example=vscode,example=sublime,example=kakoune,example=helix,example=xcode"`
+	EditPreset string `yaml:"editPreset,omitempty" jsonschema:"example=vim,example=nvim,example=emacs,example=nano,example=vscode,example=sublime,example=kakoune,example=helix,example=xcode,example=zed"`
 
 	// Command for opening a file, as if the file is double-clicked. Should
 	// contain "{{filename}}", but doesn't support "{{line}}".
