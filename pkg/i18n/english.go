d2b6f938587930d1eefe160736ccd5df784f5511
Stefan Haller <stefan@haller-berlin.de>
2024-09-12 19:02:38 +0200

    Remove unused texts

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -472,7 +472,6 @@ type TranslationSet struct {
 	RemoveRemoteTooltip                   string
 	RemoveRemotePrompt                    string
 	DeleteRemoteBranch                    string
-	DeleteRemoteBranchMessage             string
 	DeleteRemoteBranchTooltip             string
 	SetAsUpstream                         string
 	SetAsUpstreamTooltip                  string
@@ -849,7 +848,6 @@ type Actions struct {
 	CheckoutBranch                    string
 	ForceCheckoutBranch               string
 	DeleteLocalBranch                 string
-	DeleteBranch                      string
 	Merge                             string
 	SquashMerge                       string
 	RebaseBranch                      string
@@ -1463,7 +1461,6 @@ func EnglishTranslationSet() *TranslationSet {
 		RemoveRemoteTooltip:                  `Remove the selected remote. Any local branches tracking a remote branch from the remote will be unaffected.`,
 		RemoveRemotePrompt:                   "Are you sure you want to remove remote",
 		DeleteRemoteBranch:                   "Delete remote branch",
-		DeleteRemoteBranchMessage:            "Are you sure you want to delete remote branch",
 		DeleteRemoteBranchTooltip:            "Delete the remote branch from the remote.",
 		SetAsUpstream:                        "Set as upstream",
 		SetAsUpstreamTooltip:                 "Set the selected remote branch as the upstream of the checked-out branch.",
@@ -1797,7 +1794,6 @@ func EnglishTranslationSet() *TranslationSet {
 			CheckoutBranch:                 "Checkout branch",
 			ForceCheckoutBranch:            "Force checkout branch",
 			DeleteLocalBranch:              "Delete local branch",
-			DeleteBranch:                   "Delete branch",
 			Merge:                          "Merge",
 			SquashMerge:                    "Squash merge",
 			RebaseBranch:                   "Rebase branch",
