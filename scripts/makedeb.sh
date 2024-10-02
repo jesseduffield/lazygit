b6bcac347c19995121d38dd85499664e8df40f5c
FOSSMan <137928236+0xFOSSMan@users.noreply.github.com>
2024-10-02 21:12:22 +0200

    Update makedeb.sh
    renamed directory name to resolve a conflict

--- scripts/makedeb.sh
+++ scripts/makedeb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-mkdir -p lazygit/{DEBIAN,usr/local/bin}
-cat << EOF > lazygit/DEBIAN/control
+mkdir -p lazygit_deb/{DEBIAN,usr/local/bin}
+cat << EOF > lazygit_deb/DEBIAN/control
 Package: lazygit
 Version: 0.44.1
 Maintainer: jesseduffield
 Architecture: amd64
 Description: simple terminal UI for git commands
 EOF
-cp ../lazygit lazygit/usr/local/bin/
-dpkg-deb --build lazygit
+cp ../lazygit lazygit_deb/usr/local/bin/
+dpkg-deb --build lazygit_deb
+mv lazygit_deb.deb lazygit.deb
