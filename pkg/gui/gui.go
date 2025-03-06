e0e0a8807872f4d94765785ffbcb2905d7399405
Chris McDonnell <c.a.mcdonn@gmail.com>
2025-03-05 19:38:14 -0500

    Add keybinding to cancel most recent pending task

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -156,6 +156,10 @@ func (self *StateAccessor) GetRepoPathStack() *utils.StringStack {
 	return self.gui.RepoPathStack
 }
 
+func (self *StateAccessor) GetPendingTasks() []*gocui.PendingTask {
+	return self.gui.g.PendingTasks()
+}
+
 func (self *StateAccessor) GetUpdating() bool {
 	return self.gui.Updating
 }
