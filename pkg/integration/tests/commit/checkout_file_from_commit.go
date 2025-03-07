5f43027fddb3fff5829b214d474d43b3fa60a42a
Stefan Haller <stefan@haller-berlin.de>
2025-03-07 08:57:50 +0100

    Add test for checking out a file from an old commit
    This works, we just didn't have a test for it.

---
+++ pkg/integration/tests/commit/checkout_file_from_commit.go
@@ -0,0 +1,45 @@
+package commit
+
+import (
+	"github.com/jesseduffield/lazygit/pkg/config"
+	. "github.com/jesseduffield/lazygit/pkg/integration/components"
+)
+
+var CheckoutFileFromCommit = NewIntegrationTest(NewIntegrationTestArgs{
+	Description:  "Checkout an individual file from a commit",
+	ExtraCmdArgs: []string{},
+	Skip:         false,
+	SetupConfig:  func(config *config.AppConfig) {},
+	SetupRepo: func(shell *Shell) {
+		shell.CreateFileAndAdd("file", "one\n")
+		shell.Commit("one")
+		shell.UpdateFileAndAdd("file", "one\ntwo\n")
+		shell.Commit("two")
+	},
+	Run: func(t *TestDriver, keys config.KeybindingConfig) {
+		t.Views().Commits().
+			Focus().
+			Lines(
+				Contains("two").IsSelected(),
+				Contains("one"),
+			).
+			SelectNextItem().
+			PressEnter()
+
+		t.Views().CommitFiles().
+			IsFocused().
+			Lines(
+				Contains("file").IsSelected(),
+			).
+			Press(keys.CommitFiles.CheckoutCommitFile)
+
+		t.Views().Files().
+			Focus().
+			Lines(
+				Contains("M  file"),
+			)
+
+		t.FileSystem().FileContent("file", Equals("one\n"))
+	},
+},
+)
