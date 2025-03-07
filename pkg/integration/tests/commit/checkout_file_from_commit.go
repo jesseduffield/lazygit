f658627c9b98c608d8fdb03e5f68e326645913b1
Stefan Haller <stefan@haller-berlin.de>
2025-03-07 09:02:17 +0100

    Extend test to have a post-checkout hook
    As you can see, the hook is not being called.

--- pkg/integration/tests/commit/checkout_file_from_commit.go
+++ pkg/integration/tests/commit/checkout_file_from_commit.go
@@ -5,12 +5,20 @@ import (
 	. "github.com/jesseduffield/lazygit/pkg/integration/components"
 )
 
+var postCheckoutHook = `#!/bin/bash
+
+echo "post-checkout hook called" > hook-result
+`
+
 var CheckoutFileFromCommit = NewIntegrationTest(NewIntegrationTestArgs{
 	Description:  "Checkout an individual file from a commit",
 	ExtraCmdArgs: []string{},
 	Skip:         false,
 	SetupConfig:  func(config *config.AppConfig) {},
 	SetupRepo: func(shell *Shell) {
+		shell.CreateFile(".git/hooks/post-checkout", postCheckoutHook)
+		shell.MakeExecutable(".git/hooks/post-checkout")
+
 		shell.CreateFileAndAdd("file", "one\n")
 		shell.Commit("one")
 		shell.UpdateFileAndAdd("file", "one\ntwo\n")
@@ -37,9 +45,16 @@ var CheckoutFileFromCommit = NewIntegrationTest(NewIntegrationTestArgs{
 			Focus().
 			Lines(
 				Contains("M  file"),
+				/* EXPECTED:
+				Contains("?? hook-result"),
+				*/
 			)
 
 		t.FileSystem().FileContent("file", Equals("one\n"))
+		/* EXPECTED:
+		t.FileSystem().FileContent("hook-result", Equals("post-checkout hook called\n"))
+		ACTUAL: */
+		t.FileSystem().PathNotPresent("hook-result")
 	},
 },
 )
