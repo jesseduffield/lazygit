6175b1fa9c3e20535dac57596897e1e57c33fa87
Stefan Haller <stefan@haller-berlin.de>
2025-03-09 18:15:48 +0100

    Add config os.shellFunctionsFile

--- pkg/config/user_config.go
+++ pkg/config/user_config.go
@@ -589,6 +589,10 @@ type OSConfig struct {
 	// See https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md#custom-command-for-copying-to-and-pasting-from-clipboard
 	ReadFromClipboardCmd string `yaml:"readFromClipboardCmd,omitempty"`
 
+	// A shell startup file containing shell aliases or shell functions. This will be sourced before running any shell commands, so that shell functions are available in the `:` command prompt or even in custom commands.
+	// See https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md#using-aliases-or-functions-in-shell-commands
+	ShellFunctionsFile string `yaml:"shellFunctionsFile"`
+
 	// --------
 
 	// The following configs are all deprecated and kept for backward
