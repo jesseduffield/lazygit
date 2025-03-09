01ce79ee49dc87200a80d0813adb8fbf9fe3c079
Stefan Haller <stefan@haller-berlin.de>
2025-03-09 18:03:44 +0100

    Revert commits related to using an interactive shell for running shell commands
    This reverts commits f28b6f439d, dbd407c01d, 5fac40c129, and 5a3049485c.

--- pkg/commands/oscommands/os_default_platform.go
+++ pkg/commands/oscommands/os_default_platform.go
@@ -4,42 +4,15 @@
 package oscommands
 
 import (
-	"os"
 	"runtime"
-	"strings"
 )
 
 func GetPlatform() *Platform {
-	shell := getUserShell()
-
-	interactiveShell := shell
-	interactiveShellArg := "-i"
-	interactiveShellExit := "; exit $?"
-
-	if strings.HasSuffix(shell, "fish") {
-		interactiveShellExit = "; exit $status"
-	} else if !(strings.HasSuffix(shell, "bash") || strings.HasSuffix(shell, "zsh")) {
-		interactiveShell = "bash"
-		interactiveShellArg = ""
-		interactiveShellExit = ""
-	}
-
 	return &Platform{
-		OS:                   runtime.GOOS,
-		Shell:                "bash",
-		InteractiveShell:     interactiveShell,
-		ShellArg:             "-c",
-		InteractiveShellArg:  interactiveShellArg,
-		InteractiveShellExit: interactiveShellExit,
-		OpenCommand:          "open {{filename}}",
-		OpenLinkCommand:      "open {{link}}",
+		OS:              runtime.GOOS,
+		Shell:           "bash",
+		ShellArg:        "-c",
+		OpenCommand:     "open {{filename}}",
+		OpenLinkCommand: "open {{link}}",
 	}
-}
-
-func getUserShell() string {
-	if shell := os.Getenv("SHELL"); shell != "" {
-		return shell
-	}
-
-	return "bash"
 }
