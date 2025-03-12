36f4fe0c67aabcb5b7ba2e18085a956ffb95dbf6
PhatPhuckDave <david.majdandzic@hotmail.com>
2025-03-13 00:01:15 +0100

    Implement "Ignore by file extension" command
    That works exactly like the "ignore" command
    But instead of ignoring a file it ignores *.<extension> of the selected
    file

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -252,6 +252,7 @@ type TranslationSet struct {
 	OpenFileTooltip                       string
 	OpenInEditor                          string
 	IgnoreFile                            string
+	IgnoreFileExtension                   string
 	ExcludeFile                           string
 	RefreshFiles                          string
 	Merge                                 string
@@ -1291,6 +1292,7 @@ func EnglishTranslationSet() *TranslationSet {
 		OpenFileTooltip:                      "Open file in default application.",
 		OpenInEditor:                         "Open in editor",
 		IgnoreFile:                           `Add to .gitignore`,
+		IgnoreFileExtension:                  `Add extension to .gitignore`,
 		ExcludeFile:                          `Add to .git/info/exclude`,
 		RefreshFiles:                         `Refresh files`,
 		Merge:                                `Merge`,
