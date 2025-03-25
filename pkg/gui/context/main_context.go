cbd97d310ed61f3c636a3fa1f5269a3876ba1360
Stefan Haller <stefan@haller-berlin.de>
2025-03-25 14:31:03 +0100

    Add class MainContext
    In this state of the code it isn't worth much because it's not any more than a
    SimpleContext, but we'll add things to it later in the branch.

---
+++ pkg/gui/context/main_context.go
@@ -0,0 +1,29 @@
+package context
+
+import (
+	"github.com/jesseduffield/gocui"
+	"github.com/jesseduffield/lazygit/pkg/gui/types"
+)
+
+type MainContext struct {
+	*SimpleContext
+}
+
+func NewMainContext(
+	view *gocui.View,
+	windowName string,
+	key types.ContextKey,
+) *MainContext {
+	ctx := &MainContext{
+		SimpleContext: NewSimpleContext(
+			NewBaseContext(NewBaseContextOpts{
+				Kind:       types.MAIN_CONTEXT,
+				View:       view,
+				WindowName: windowName,
+				Key:        key,
+				Focusable:  false,
+			})),
+	}
+
+	return ctx
+}
