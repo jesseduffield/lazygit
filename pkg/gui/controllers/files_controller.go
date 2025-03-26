6f8fd47ecbc0894823be028cbdbe1f1bcf7cb6c2
Stefan Haller <stefan@haller-berlin.de>
2025-03-26 12:39:39 +0100

    Allow clicking in focused main view to go to staging
    Only works if a file is selected.

--- pkg/gui/controllers/files_controller.go
+++ pkg/gui/controllers/files_controller.go
@@ -290,6 +290,16 @@ func (self *FilesController) GetOnClick() func() error {
 	})
 }
 
+func (self *FilesController) GetOnClickFocusedMainView() func(mainViewName string, clickedLineIdx int) error {
+	return func(mainViewName string, clickedLineIdx int) error {
+		node := self.getSelectedItem()
+		if node != nil && node.File != nil {
+			return self.EnterFile(types.OnFocusOpts{ClickedWindowName: mainViewName, ClickedViewLineIdx: clickedLineIdx})
+		}
+		return nil
+	}
+}
+
 // if we are dealing with a status for which there is no key in this map,
 // then we won't optimistically render: we'll just let `git status` tell
 // us what the new status is.
@@ -512,7 +522,8 @@ func (self *FilesController) EnterFile(opts types.OnFocusOpts) error {
 		return errors.New(self.c.Tr.FileStagingRequirements)
 	}
 
-	self.c.Context().Push(self.c.Contexts().Staging, opts)
+	context := lo.Ternary(opts.ClickedWindowName == "secondary", self.c.Contexts().StagingSecondary, self.c.Contexts().Staging)
+	self.c.Context().Push(context, opts)
 	return nil
 }
 
