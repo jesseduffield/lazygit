f411dcc6addd496b6806645c0e746074feff56c6
Azhar Madar Shaik <azarshaik454@gmail.com>
2025-04-01 11:26:14 +0530

    escape files that start with '#' or '!'

--- pkg/commands/git_commands/working_tree.go
+++ pkg/commands/git_commands/working_tree.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/go-errors/errors"
 	"github.com/jesseduffield/lazygit/pkg/commands/models"
@@ -230,15 +231,23 @@ func (self *WorkingTreeCommands) DiscardUnstagedFileChanges(file *models.File) e
 	return self.cmd.New(cmdArgs).Run()
 }
 
+func escapeFilename(filename string) string {
+	escapedFilename := filename
+	if strings.HasPrefix(filename, "#") || strings.HasPrefix(filename, "!") {
+		escapedFilename = "\\" + filename
+	}
+	return escapedFilename
+}
+
 // Ignore adds a file to the gitignore for the repo
 func (self *WorkingTreeCommands) Ignore(filename string) error {
-	return self.os.AppendLineToFile(".gitignore", filename)
+	return self.os.AppendLineToFile(".gitignore", escapeFilename(filename))
 }
 
 // Exclude adds a file to the .git/info/exclude for the repo
 func (self *WorkingTreeCommands) Exclude(filename string) error {
 	excludeFile := filepath.Join(self.repoPaths.repoGitDirPath, "info", "exclude")
-	return self.os.AppendLineToFile(excludeFile, filename)
+	return self.os.AppendLineToFile(excludeFile, escapeFilename(filename))
 }
 
 // WorktreeFileDiff returns the diff of a file
