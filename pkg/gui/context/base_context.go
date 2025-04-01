bda5576bd3028e58ce55728c59674b2566b78e30
Stefan Haller <stefan@haller-berlin.de>
2025-04-01 16:14:10 +0200

    Add user config gui.showSelectionInFocusedMainView

--- pkg/gui/context/base_context.go
+++ pkg/gui/context/base_context.go
@@ -230,3 +230,7 @@ func (self *BaseContext) Title() string {
 func (self *BaseContext) TotalContentHeight() int {
 	return self.view.ViewLinesHeight()
 }
+
+func (self *BaseContext) SetHighlightOnFocus(value bool) {
+	self.highlightOnFocus = value
+}
