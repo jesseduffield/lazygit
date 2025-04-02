16dd60ce62c7a3e932e0cd3e3d272c52e63beeaa
Stefan Haller <stefan@haller-berlin.de>
2025-04-02 14:50:53 +0200

    Cleanup: remove unnecessary indirection

--- pkg/gui/controllers/helpers/merge_and_rebase_helper.go
+++ pkg/gui/controllers/helpers/merge_and_rebase_helper.go
@@ -301,7 +301,7 @@ func (self *MergeAndRebaseHelper) RebaseOntoRef(ref string) error {
 		disabledReason = &types.DisabledReason{Text: self.c.Tr.CantRebaseOntoSelf}
 	}
 
-	baseBranch, err := self.c.Git().Loaders.BranchLoader.GetBaseBranch(checkedOutBranch, self.refsHelper.c.Model().MainBranches)
+	baseBranch, err := self.c.Git().Loaders.BranchLoader.GetBaseBranch(checkedOutBranch, self.c.Model().MainBranches)
 	if err != nil {
 		return err
 	}
