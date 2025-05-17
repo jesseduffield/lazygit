12ed50464b668a37644caaa78576a2faca3f8e5b
Stefan Haller <stefan@haller-berlin.de>
2025-05-17 11:47:49 +0200

    Cleanup: pass target context to focusMainView directly
    It's a bit silly to pass a window name and then call a function to get the
    corresponding context, when we can simply pass the context directly.

--- pkg/gui/controllers/switch_to_focused_main_view_controller.go
+++ pkg/gui/controllers/switch_to_focused_main_view_controller.go
@@ -60,19 +60,18 @@ func (self *SwitchToFocusedMainViewController) Context() types.Context {
 }
 
 func (self *SwitchToFocusedMainViewController) onClickMain(opts gocui.ViewMouseBindingOpts) error {
-	return self.focusMainView("main")
+	return self.focusMainView(self.c.Contexts().Normal)
 }
 
 func (self *SwitchToFocusedMainViewController) onClickSecondary(opts gocui.ViewMouseBindingOpts) error {
-	return self.focusMainView("secondary")
+	return self.focusMainView(self.c.Contexts().NormalSecondary)
 }
 
 func (self *SwitchToFocusedMainViewController) handleFocusMainView() error {
-	return self.focusMainView("main")
+	return self.focusMainView(self.c.Contexts().Normal)
 }
 
-func (self *SwitchToFocusedMainViewController) focusMainView(mainViewName string) error {
-	mainViewContext := self.c.Helpers().Window.GetContextForWindow(mainViewName)
+func (self *SwitchToFocusedMainViewController) focusMainView(mainViewContext types.Context) error {
 	mainViewContext.SetParentContext(self.context)
 	if context, ok := mainViewContext.(types.ISearchableContext); ok {
 		context.ClearSearchString()
