823aa640b9d66964f28a7852bdd743bb4c8f8763
Stefan Haller <stefan@haller-berlin.de>
2025-07-01 12:57:16 +0200

    Add test for moving patch to index when there's a modified file
    This is functionality that works already, we only add the test for more complete
    test coverage. However, there's a detail problem, and the test demonstrates
    this: we keep the stash even if there was no conflict. We'll fix this next.

--- pkg/integration/tests/test_list.go
+++ pkg/integration/tests/test_list.go
@@ -310,6 +310,7 @@ var tests = []*components.IntegrationTest{
 	patch_building.MoveToIndexPartOfAdjacentAddedLines,
 	patch_building.MoveToIndexPartial,
 	patch_building.MoveToIndexWithConflict,
+	patch_building.MoveToIndexWithModifiedFile,
 	patch_building.MoveToIndexWorksEvenIfNoprefixIsSet,
 	patch_building.MoveToLaterCommit,
 	patch_building.MoveToLaterCommitPartialHunk,
