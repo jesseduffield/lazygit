a6096f470243249f8af33fca49d501fdf8e03c39
Stefan Haller <stefan@haller-berlin.de>
2025-07-04 11:55:41 +0200

    In hunk staging mode, select blocks of changes rather than actual hunks
    Also, pressing right or left arrow moves between blocks of changes rather than
    actual hunks. I find this to be much more useful.

--- pkg/integration/tests/staging/stage_hunks.go
+++ pkg/integration/tests/staging/stage_hunks.go
@@ -11,11 +11,10 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 	Skip:         false,
 	SetupConfig:  func(config *config.AppConfig) {},
 	SetupRepo: func(shell *Shell) {
-		// need to be working with a few lines so that git perceives it as two separate hunks
-		shell.CreateFileAndAdd("file1", "1a\n2a\n3a\n4a\n5a\n6a\n7a\n8a\n9a\n10a\n11a\n12a\n13a\n14a\n15a")
+		shell.CreateFileAndAdd("file1", "1a\n2a\n3a\n4a\n5a\n6a\n7a\n8a")
 		shell.Commit("one")
 
-		shell.UpdateFile("file1", "1a\n2a\n3b\n4a\n5a\n6a\n7a\n8a\n9a\n10a\n11a\n12a\n13b\n14a\n15a")
+		shell.UpdateFile("file1", "1a\n2a\n3b\n4a\n5a\n6b\n7a\n8a")
 
 		// hunk looks like:
 		// diff --git a/file1 b/file1
@@ -29,15 +28,10 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 		// +3b
 		//  4a
 		//  5a
-		//  6a
-		// @@ -10,6 +10,6 @@
-		//  10a
-		//  11a
-		//  12a
-		// -13a
-		// +13b
-		//  14a
-		//  15a
+		// -6a
+		// +6b
+		//  7a
+		//  8a
 		// \ No newline at end of file
 	},
 	Run: func(t *TestDriver, keys config.KeybindingConfig) {
@@ -55,43 +49,23 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 			).
 			Press(keys.Universal.NextBlock).
 			SelectedLines(
-				Contains("-13a"),
+				Contains("-6a"),
 			).
 			Press(keys.Main.ToggleSelectHunk).
 			SelectedLines(
-				Contains("@@ -10,6 +10,6 @@"),
-				Contains(" 10a"),
-				Contains(" 11a"),
-				Contains(" 12a"),
-				Contains("-13a"),
-				Contains("+13b"),
-				Contains(" 14a"),
-				Contains(" 15a"),
-				Contains(`\ No newline at end of file`),
+				Contains("-6a"),
+				Contains("+6b"),
 			).
 			// when in hunk mode, pressing up/down moves us up/down by a hunk
 			SelectPreviousItem().
 			SelectedLines(
-				Contains(`@@ -1,6 +1,6 @@`),
-				Contains(` 1a`),
-				Contains(` 2a`),
 				Contains(`-3a`),
 				Contains(`+3b`),
-				Contains(` 4a`),
-				Contains(` 5a`),
-				Contains(` 6a`),
 			).
 			SelectNextItem().
 			SelectedLines(
-				Contains("@@ -10,6 +10,6 @@"),
-				Contains(" 10a"),
-				Contains(" 11a"),
-				Contains(" 12a"),
-				Contains("-13a"),
-				Contains("+13b"),
-				Contains(" 14a"),
-				Contains(" 15a"),
-				Contains(`\ No newline at end of file`),
+				Contains("-6a"),
+				Contains("+6b"),
 			).
 			// stage the second hunk
 			PressPrimaryAction().
@@ -102,8 +76,8 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 			Tap(func() {
 				t.Views().StagingSecondary().
 					ContainsLines(
-						Contains("-13a"),
-						Contains("+13b"),
+						Contains("-6a"),
+						Contains("+6b"),
 					)
 			}).
 			Press(keys.Universal.TogglePanel)
@@ -112,11 +86,11 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 			IsFocused().
 			// after toggling panel, we're back to only having selected a single line
 			SelectedLines(
-				Contains("-13a"),
+				Contains("-6a"),
 			).
 			PressPrimaryAction().
 			SelectedLines(
-				Contains("+13b"),
+				Contains("+6b"),
 			).
 			PressPrimaryAction().
 			IsEmpty()
@@ -128,14 +102,8 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 			).
 			Press(keys.Main.ToggleSelectHunk).
 			SelectedLines(
-				Contains(`@@ -1,6 +1,6 @@`),
-				Contains(` 1a`),
-				Contains(` 2a`),
 				Contains(`-3a`),
 				Contains(`+3b`),
-				Contains(` 4a`),
-				Contains(` 5a`),
-				Contains(` 6a`),
 			).
 			Press(keys.Universal.Remove).
 			Tap(func() {
@@ -143,15 +111,8 @@ var StageHunks = NewIntegrationTest(NewIntegrationTestArgs{
 			}).
 			Content(DoesNotContain("-3a").DoesNotContain("+3b")).
 			SelectedLines(
-				Contains("@@ -10,6 +10,6 @@"),
-				Contains(" 10a"),
-				Contains(" 11a"),
-				Contains(" 12a"),
-				Contains("-13a"),
-				Contains("+13b"),
-				Contains(" 14a"),
-				Contains(" 15a"),
-				Contains(`\ No newline at end of file`),
+				Contains("-6a"),
+				Contains("+6b"),
 			)
 	},
 })
