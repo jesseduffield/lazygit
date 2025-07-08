28bb6a0e803de2f39f27d31ebf0dde832e887056
Stefan Haller <stefan@haller-berlin.de>
2025-07-08 15:14:45 +0200

    Remove some unused code

--- pkg/gui/context_config.go
+++ pkg/gui/context_config.go
@@ -13,14 +13,6 @@ func (gui *Gui) contextTree() *context.ContextTree {
 	return context.NewContextTree(contextCommon)
 }
 
-// using this wrapper for when an onFocus function doesn't care about any potential
-// props that could be passed
-func OnFocusWrapper(f func() error) func(opts types.OnFocusOpts) error {
-	return func(opts types.OnFocusOpts) error {
-		return f()
-	}
-}
-
 func (gui *Gui) defaultSideContext() types.Context {
 	if gui.State.Modes.Filtering.Active() {
 		return gui.State.Contexts.LocalCommits
