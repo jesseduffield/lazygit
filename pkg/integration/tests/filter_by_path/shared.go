4981419ba95d0dc406a52c2938f28e89feba165c
Stefan Haller <stefan@haller-berlin.de>
2025-07-09 15:12:03 +0200

    Fix stale main view content when entering/exiting filtering view
    When entering filtering we would only call FocusLine, which takes care of
    highlighting the selected line in the commits list, but not of re-rendering the
    main view. HandleFocus does that.
    
    When exiting filtering, the HandleFocus call was missing entirely.
    
    The tests needed to be reworked a little bit to make this testable.

--- pkg/integration/tests/filter_by_path/shared.go
+++ pkg/integration/tests/filter_by_path/shared.go
@@ -6,14 +6,13 @@ import (
 
 func commonSetup(shell *Shell) {
 	shell.CreateFileAndAdd("filterFile", "original filterFile content")
+	shell.Commit("only filterFile")
 	shell.CreateFileAndAdd("otherFile", "original otherFile content")
-	shell.Commit("both files")
-
-	shell.UpdateFileAndAdd("otherFile", "new otherFile content")
 	shell.Commit("only otherFile")
 
+	shell.UpdateFileAndAdd("otherFile", "new otherFile content")
 	shell.UpdateFileAndAdd("filterFile", "new filterFile content")
-	shell.Commit("only filterFile")
+	shell.Commit("both files")
 
 	shell.EmptyCommit("none of the two")
 }
@@ -24,14 +23,18 @@ func postFilterTest(t *TestDriver) {
 	t.Views().Commits().
 		IsFocused().
 		Lines(
-			Contains(`only filterFile`).IsSelected(),
-			Contains(`both files`),
-		).
-		SelectNextItem()
+			Contains(`both files`).IsSelected(),
+			Contains(`only filterFile`),
+		)
 
 	// we only show the filtered file's changes in the main view
 	t.Views().Main().
-		Content(Contains("filterFile").DoesNotContain("otherFile"))
+		ContainsLines(
+			Equals("    both files"),
+			Equals("---"),
+			Equals(" filterFile | 2 +-"),
+			Equals(" 1 file changed, 1 insertion(+), 1 deletion(-)"),
+		)
 
 	t.Views().Commits().
 		PressEnter()
