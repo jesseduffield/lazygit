5cb80b8635556011ad62df586e1757f0cd84e675
Stefan Haller <stefan@haller-berlin.de>
2025-08-25 19:12:38 +0200

    Fix command log for stash commands
    With the exception of Rename Stash, they would all just log "Stash".

--- pkg/gui/controllers/stash_controller.go
+++ pkg/gui/controllers/stash_controller.go
@@ -112,7 +112,7 @@ func (self *StashController) handleStashApply(stashEntry *models.StashEntry) err
 			Title:  self.c.Tr.StashApply,
 			Prompt: self.c.Tr.SureApplyStashEntry,
 			HandleConfirm: func() error {
-				self.c.LogAction(self.c.Tr.Actions.Stash)
+				self.c.LogAction(self.c.Tr.Actions.ApplyStash)
 				err := self.c.Git().Stash.Apply(stashEntry.Index)
 				self.postStashRefresh()
 				if err != nil {
@@ -128,7 +128,7 @@ func (self *StashController) handleStashApply(stashEntry *models.StashEntry) err
 
 func (self *StashController) handleStashPop(stashEntry *models.StashEntry) error {
 	pop := func() error {
-		self.c.LogAction(self.c.Tr.Actions.Stash)
+		self.c.LogAction(self.c.Tr.Actions.PopStash)
 		err := self.c.Git().Stash.Pop(stashEntry.Index)
 		self.postStashRefresh()
 		if err != nil {
@@ -160,7 +160,7 @@ func (self *StashController) handleStashDrop(stashEntries []*models.StashEntry) 
 		Title:  self.c.Tr.StashDrop,
 		Prompt: self.c.Tr.SureDropStashEntry,
 		HandleConfirm: func() error {
-			self.c.LogAction(self.c.Tr.Actions.Stash)
+			self.c.LogAction(self.c.Tr.Actions.DropStash)
 			for i := len(stashEntries) - 1; i >= 0; i-- {
 				err := self.c.Git().Stash.Drop(stashEntries[i].Index)
 				self.c.Refresh(types.RefreshOptions{Scope: []types.RefreshableView{types.STASH}})
