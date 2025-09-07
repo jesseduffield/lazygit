jesseduffield/lazygit

commit b3a3410a1a98aa334bdf58ac7a695d4403281371

Remove keybindings for menu items that are the same as the menu confirm key
This is needed when remapping the confirmMenu key to, say, "y", and there's a
menu that has an item with a "y" binding. This already worked correctly (confirm
takes precedence, as desired), but it's still confusing to see the item binding.

pkg/gui/menu_panel.go
@@ -3,6 +3,7 @@ package gui
 import (
 	"fmt"
 
+	"github.com/jesseduffield/lazygit/pkg/gui/keybindings"
 	"github.com/jesseduffield/lazygit/pkg/gui/types"
 	"github.com/jesseduffield/lazygit/pkg/theme"
 )
@@ -20,6 +21,7 @@ func (gui *Gui) createMenu(opts types.CreateMenuOptions) error {
 	}
 
 	maxColumnSize := 1
+	confirmKey := keybindings.GetKey(gui.c.UserConfig().Keybinding.Universal.ConfirmMenu)
 
 	for _, item := range opts.Items {
 		if item.LabelColumns == nil {
@@ -31,6 +33,11 @@ func (gui *Gui) createMenu(opts types.CreateMenuOptions) error {
 		}
 
 		maxColumnSize = max(maxColumnSize, len(item.LabelColumns))
+
+		// Remove all item keybindings that are the same as the confirm binding
+		if item.Key == confirmKey {
+			item.Key = nil
+		}
 	}
 
 	for _, item := range opts.Items {

commit 26096d9dd90ba3af378533f173f8feb1133ada49

Don't hide keybindings that match the confirmMenu key in the keybindings menu
In all other menus besides the keybindings menu it makes sense to hide
keybindings that match the confirmMenu binding. This is important to make it
clear which action will be triggered when you press the key.

In the keybindings menu this is different; the main purpose of that menu is not
to allow triggering commands by their key while the menu is open, but to serve
as a reference for what the keybindings are when it is not open. Because of
this, it is more important to show all bindings in this menu, even if they
conflict with the confirmMenu key.

This fixes a regression introduced in b3a3410a1a98.

pkg/gui/controllers/options_menu_action.go
@@ -51,6 +51,7 @@ func (self *OptionsMenuAction) Call() error {
 		HideCancel:                true,
 		ColumnAlignment:           []utils.Alignment{utils.AlignRight, utils.AlignLeft},
 		AllowFilteringKeybindings: true,
+		KeepConfirmKeybindings:    true,
 	})
 }
 

pkg/gui/menu_panel.go
@@ -35,7 +35,7 @@ func (gui *Gui) createMenu(opts types.CreateMenuOptions) error {
 		maxColumnSize = max(maxColumnSize, len(item.LabelColumns))
 
 		// Remove all item keybindings that are the same as the confirm binding
-		if item.Key == confirmKey {
+		if item.Key == confirmKey && !opts.KeepConfirmKeybindings {
 			item.Key = nil
 		}
 	}

pkg/gui/types/common.go
@@ -156,6 +156,7 @@ type CreateMenuOptions struct {
 	HideCancel                bool
 	ColumnAlignment           []utils.Alignment
 	AllowFilteringKeybindings bool
+	KeepConfirmKeybindings    bool // if true, the keybindings that match the confirm binding will not be removed from menu items
 }
 
 type CreatePopupPanelOpts struct {