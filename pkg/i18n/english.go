9aa268c1f44a46e7be1a864e65b540e1f4716142
Stefan Haller <stefan@haller-berlin.de>
2025-12-11 21:09:57 +0100

    Cleanup: use spaces instead of tab

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -2210,7 +2210,7 @@ gui:
 keybinding:
   universal:
     suspendApp: <disabled>
-	redo: <c-z>
+    redo: <c-z>
 
 - The 'git.paging.useConfig' option has been removed. If you were relying on it to configure your pager, you'll have to explicitly set the pager again using the git.paging.pager' option.
 `,
