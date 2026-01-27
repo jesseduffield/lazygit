42970c332c8b913cbd2bdda44bfdb2daf7d8a8c0
Calo <calobozan@gmail.com>
2026-01-27 12:45:37 -0800

    feat: add 'copy branch URL to clipboard' option
    Adds a new keybinding (ctrl+u by default) to copy the branch URL to the
    clipboard, similar to the existing 'copy pull request URL' feature.
    
    Closes #1959
    
    - Add branchURL template to ServiceDefinition for all supported providers:
      - GitHub: /tree/{{.BranchName}}
      - GitLab: /-/tree/{{.BranchName}}
      - Bitbucket: /src/{{.BranchName}}
      - Azure DevOps: ?version=GB{{.BranchName}}
      - Bitbucket Server: /browse?at={{.BranchName}}
      - Gitea: /src/branch/{{.BranchName}}
      - Codeberg: /src/branch/{{.BranchName}}
    - Add GetBranchURL method to HostingServiceMgr and HostHelper
    - Add copyBranchURL handler in BranchesController
    - Add keybinding config (Branches.CopyBranchURL)
    - Add translations for UI strings
    - Add tests for GetBranchURL

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -276,6 +276,7 @@ type TranslationSet struct {
 	AllBranchesLogGraph                   string
 	UnsupportedGitService                 string
 	CopyPullRequestURL                    string
+	CopyBranchURL                         string
 	NoBranchOnRemote                      string
 	Fetch                                 string
 	FetchTooltip                          string
@@ -725,6 +726,7 @@ type TranslationSet struct {
 	SuggestionsSubtitle                      string
 	ExtrasTitle                              string
 	PullRequestURLCopiedToClipboard          string
+	BranchURLCopiedToClipboard               string
 	CommitDiffCopiedToClipboard              string
 	CommitURLCopiedToClipboard               string
 	CommitMessageCopiedToClipboard           string
@@ -1059,6 +1061,7 @@ type Actions struct {
 	Undo                             string
 	Redo                             string
 	CopyPullRequestURL               string
+	CopyBranchURL                    string
 	OpenMergeTool                    string
 	OpenCommitInBrowser              string
 	OpenPullRequest                  string
@@ -1372,6 +1375,7 @@ func EnglishTranslationSet() *TranslationSet {
 		UnsupportedGitService:                `Unsupported git service`,
 		CreatePullRequest:                    `Create pull request`,
 		CopyPullRequestURL:                   `Copy pull request URL to clipboard`,
+		CopyBranchURL:                        `Copy branch URL to clipboard`,
 		NoBranchOnRemote:                     `This branch doesn't exist on remote. You need to push it to remote first.`,
 		Fetch:                                `Fetch`,
 		FetchTooltip:                         "Fetch changes from remote.",
@@ -1825,6 +1829,7 @@ func EnglishTranslationSet() *TranslationSet {
 		SuggestionsSubtitle:                      "(press %s to delete, %s to edit)",
 		ExtrasTitle:                              "Command log",
 		PullRequestURLCopiedToClipboard:          "Pull request URL copied to clipboard",
+		BranchURLCopiedToClipboard:               "Branch URL copied to clipboard",
 		CommitDiffCopiedToClipboard:              "Commit diff copied to clipboard",
 		CommitURLCopiedToClipboard:               "Commit URL copied to clipboard",
 		CommitMessageCopiedToClipboard:           "Commit message copied to clipboard",
@@ -2120,6 +2125,7 @@ func EnglishTranslationSet() *TranslationSet {
 			Undo:                             "Undo",
 			Redo:                             "Redo",
 			CopyPullRequestURL:               "Copy pull request URL",
+			CopyBranchURL:                    "Copy branch URL",
 			OpenMergeTool:                    "Open merge tool",
 			OpenCommitInBrowser:              "Open commit in browser",
 			OpenPullRequest:                  "Open pull request in browser",
