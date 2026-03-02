6dd7e08f4bcd56c9159661b1a3daa597d7429a6f
Shankar Tiwari <tiwarishankart@gmail.com>
2026-03-02 16:56:33 +0530

    feat: add "copy branch URL" keybinding to branches panel
    Add a new keybinding (<c-u>) in the branches panel to copy the branch's
    web URL to the clipboard (e.g. https://github.com/user/repo/tree/branch).
    Follows the same pattern as the existing "copy pull request URL" feature.
    
    Supports GitHub, GitLab, Bitbucket, Azure DevOps, Bitbucket Server,
    Gitea, and Codeberg.
    
    Closes #1959

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -281,6 +281,7 @@ type TranslationSet struct {
 	AllBranchesLogGraph                   string
 	UnsupportedGitService                 string
 	CopyPullRequestURL                    string
+	CopyBranchURL                         string
 	NoBranchOnRemote                      string
 	Fetch                                 string
 	FetchTooltip                          string
@@ -732,6 +733,7 @@ type TranslationSet struct {
 	SuggestionsSubtitle                      string
 	ExtrasTitle                              string
 	PullRequestURLCopiedToClipboard          string
+	BranchURLCopiedToClipboard               string
 	CommitDiffCopiedToClipboard              string
 	CommitURLCopiedToClipboard               string
 	CommitMessageCopiedToClipboard           string
@@ -1066,6 +1068,7 @@ type Actions struct {
 	Undo                             string
 	Redo                             string
 	CopyPullRequestURL               string
+	CopyBranchURL                    string
 	OpenMergeTool                    string
 	OpenCommitInBrowser              string
 	OpenPullRequest                  string
@@ -1384,6 +1387,7 @@ func EnglishTranslationSet() *TranslationSet {
 		UnsupportedGitService:                `Unsupported git service`,
 		CreatePullRequest:                    `Create pull request`,
 		CopyPullRequestURL:                   `Copy pull request URL to clipboard`,
+		CopyBranchURL:                        `Copy branch URL to clipboard`,
 		NoBranchOnRemote:                     `This branch doesn't exist on remote. You need to push it to remote first.`,
 		Fetch:                                `Fetch`,
 		FetchTooltip:                         "Fetch changes from remote.",
@@ -1839,6 +1843,7 @@ func EnglishTranslationSet() *TranslationSet {
 		SuggestionsSubtitle:                      "(press %s to delete, %s to edit)",
 		ExtrasTitle:                              "Command log",
 		PullRequestURLCopiedToClipboard:          "Pull request URL copied to clipboard",
+		BranchURLCopiedToClipboard:               "Branch URL copied to clipboard",
 		CommitDiffCopiedToClipboard:              "Commit diff copied to clipboard",
 		CommitURLCopiedToClipboard:               "Commit URL copied to clipboard",
 		CommitMessageCopiedToClipboard:           "Commit message copied to clipboard",
@@ -2134,6 +2139,7 @@ func EnglishTranslationSet() *TranslationSet {
 			Undo:                             "Undo",
 			Redo:                             "Redo",
 			CopyPullRequestURL:               "Copy pull request URL",
+			CopyBranchURL:                    "Copy branch URL",
 			OpenMergeTool:                    "Open merge tool",
 			OpenCommitInBrowser:              "Open commit in browser",
 			OpenPullRequest:                  "Open pull request in browser",
