55fd5f8851b8989b2cc8fa2094941c109d6f0fbf
Ruud Kamphuis <ruudk@users.noreply.github.com>
2026-03-12 08:06:38 +0100

    Add "Remove worktree and delete local branch" option to branch delete menu
    When pressing 'd' on a branch that is checked out by another worktree,
    the delete menu now shows "Remove worktree and delete local branch" as
    the first (default) option, making it easy to clean up both in one step.

--- pkg/i18n/english.go
+++ pkg/i18n/english.go
@@ -862,6 +862,7 @@ type TranslationSet struct {
 	DetachWorktreeTooltip                    string
 	Switching                                string
 	RemoveWorktree                           string
+	RemoveWorktreeAndBranch                  string
 	RemoveWorktreeTitle                      string
 	DetachWorktree                           string
 	DetachingWorktree                        string
@@ -1978,6 +1979,7 @@ func EnglishTranslationSet() *TranslationSet {
 		DetachWorktreeTooltip:                    "This will run `git checkout --detach` on the worktree so that it stops hogging the branch, but the worktree's working tree will be left alone.",
 		Switching:                                "Switching",
 		RemoveWorktree:                           "Remove worktree",
+		RemoveWorktreeAndBranch:                  "Remove worktree and delete local branch",
 		RemoveWorktreeTitle:                      "Remove worktree",
 		RemoveWorktreePrompt:                     "Are you sure you want to remove worktree '{{.worktreeName}}'?",
 		ForceRemoveWorktreePrompt:                "'{{.worktreeName}}' contains modified or untracked files, or submodules (or all of these). Are you sure you want to remove it?",
