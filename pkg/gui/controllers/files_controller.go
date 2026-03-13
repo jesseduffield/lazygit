0813934bb51ca449b86db858b59dc97fe69aa719
Tushar Muralidharan <tusharmurali.math@gmail.com>
2026-03-13 18:53:37 +1100

    Change 'Keep file' shortcut from 'k' to 'r' in merge conflict menu
    'k' conflicts with vim-style navigation for moving up in the menu. Changed to 'r' (retain) to avoid accidental selection.

--- pkg/gui/controllers/files_controller.go
+++ pkg/gui/controllers/files_controller.go
@@ -647,7 +647,7 @@ func (self *FilesController) handleNonInlineConflict(file *models.File) error {
 		OnPress: func() error {
 			return handle(self.c.Git().WorkingTree.StageFile, self.c.Tr.Actions.ResolveConflictByKeepingFile)
 		},
-		Key: 'k',
+		Key: 'r', // 'r' for retain; avoid 'k' which conflicts with navigation
 	}
 	deleteItem := &types.MenuItem{
 		Label: self.c.Tr.MergeConflictDeleteFile,
