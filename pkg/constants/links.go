e296845aaed85c3888b733435f84a3235eabb650
Stefan Haller <stefan@haller-berlin.de>
2026-07-26 19:01:10 +0200

    Adapt links

--- pkg/constants/links.go
+++ pkg/constants/links.go
@@ -1,14 +1,14 @@
 package constants
 
 type Docs struct {
-	CustomPagers      string
-	CustomCommands    string
-	CustomKeybindings string
-	Keybindings       string
-	Undoing           string
-	Config            string
-	Tutorial          string
-	CustomPatchDemo   string
+	CustomDiffRenderers string
+	CustomCommands      string
+	CustomKeybindings   string
+	Keybindings         string
+	Undoing             string
+	Config              string
+	Tutorial            string
+	CustomPatchDemo     string
 }
 
 var Links = struct {
@@ -25,13 +25,13 @@ var Links = struct {
 	Discussions: "https://github.com/jesseduffield/lazygit/discussions",
 	Releases:    "https://github.com/jesseduffield/lazygit/releases",
 	Docs: Docs{
-		CustomPagers:      "https://github.com/jesseduffield/lazygit/blob/master/docs/Custom_Pagers.md",
-		CustomKeybindings: "https://github.com/jesseduffield/lazygit/blob/master/docs/keybindings/Custom_Keybindings.md",
-		CustomCommands:    "https://github.com/jesseduffield/lazygit/wiki/Custom-Commands-Compendium",
-		Keybindings:       "https://github.com/jesseduffield/lazygit/blob/%s/docs/keybindings",
-		Undoing:           "https://github.com/jesseduffield/lazygit/blob/master/docs/Undoing.md",
-		Config:            "https://github.com/jesseduffield/lazygit/blob/%s/docs/Config.md",
-		Tutorial:          "https://youtu.be/VDXvbHZYeKY",
-		CustomPatchDemo:   "https://github.com/jesseduffield/lazygit#rebase-magic-custom-patches",
+		CustomDiffRenderers: "https://github.com/jesseduffield/lazygit/blob/master/docs/Custom_DiffRenderers.md",
+		CustomKeybindings:   "https://github.com/jesseduffield/lazygit/blob/master/docs/keybindings/Custom_Keybindings.md",
+		CustomCommands:      "https://github.com/jesseduffield/lazygit/wiki/Custom-Commands-Compendium",
+		Keybindings:         "https://github.com/jesseduffield/lazygit/blob/%s/docs/keybindings",
+		Undoing:             "https://github.com/jesseduffield/lazygit/blob/master/docs/Undoing.md",
+		Config:              "https://github.com/jesseduffield/lazygit/blob/%s/docs/Config.md",
+		Tutorial:            "https://youtu.be/VDXvbHZYeKY",
+		CustomPatchDemo:     "https://github.com/jesseduffield/lazygit#rebase-magic-custom-patches",
 	},
 }
